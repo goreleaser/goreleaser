8b90e175965929b92db5224bc56ec537ad4f125d
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-12-28 23:33:29 -0200

    tests

--- config/config_test.go
+++ config/config_test.go
@@ -8,21 +8,11 @@ import (
 func TestFixConfig(t *testing.T) {
 	assert := assert.New(t)
 	config := fix(ProjectConfig{})
-	assert.Equal("releaser", config.BinaryName)
 	assert.Equal("main.go", config.Main)
 	assert.Contains(config.FileList, "README.md")
 	assert.Contains(config.FileList, "LICENSE.md")
-	assert.Contains(config.FileList, "releaser")
 	assert.Contains(config.Build.Oses, "darwin")
 	assert.Contains(config.Build.Oses, "linux")
 	assert.Contains(config.Build.Arches, "386")
 	assert.Contains(config.Build.Arches, "amd64")
-}
-
-func TestFixBadConfig(t *testing.T) {
-	assert := assert.New(t)
-	config := fix(ProjectConfig{
-		FileList: []string{"README.txt"},
-	})
-	assert.Contains(config.FileList, "releaser")
 }
