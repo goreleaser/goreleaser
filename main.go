5f6df39badc40679b335533b0359fee4c01c7ceb
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-12-31 13:57:39 -0200

    improved main.go
    closes #1

--- main.go
+++ main.go
@@ -9,25 +9,46 @@ import (
 	"github.com/goreleaser/releaser/pipeline/build"
 	"github.com/goreleaser/releaser/pipeline/compress"
 	"github.com/goreleaser/releaser/pipeline/release"
+	"github.com/urfave/cli"
+	"os"
 )
 
 var version = "master"
 
+var pipes = []pipeline.Pipe{
+	build.Pipe{},
+	compress.Pipe{},
+	release.Pipe{},
+	brew.Pipe{},
+}
+
 func main() {
-	config, err := config.Load("goreleaser.yml")
-	if err != nil {
-		log.Fatalln("Failed to load goreleaser.yml:", err.Error())
+	var app = cli.NewApp()
+	app.Name = "release"
+	app.Version = version
+	app.Usage = "Deliver Go binaries as fast and easily as possible"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config, c",
+			Usage: "Load configuration from `FILE`",
+			Value: "goreleaser.yml",
+
+		},
 	}
-	log.Println("Releasing", config.Git.CurrentTag, "...")
-	var pipeline = []pipeline.Pipe{
-		build.Pipe{},
-		compress.Pipe{},
-		release.Pipe{},
-		brew.Pipe{},
-	}
-	for _, pipe := range pipeline {
-		if err := pipe.Run(config); err != nil {
-			log.Fatalln(pipe.Name(), "failed:", err.Error())
+	app.Action = func(c *cli.Context) (err error) {
+		var file = c.String("config")
+		config, err := config.Load(file)
+		if err != nil {
+			return cli.NewExitError(err.Error(), 1)
 		}
+		log.Println("Releasing", config.Git.CurrentTag, "...")
+		for _, pipe := range pipes {
+			if err := pipe.Run(config); err != nil {
+				return cli.NewExitError(pipe.Name()+" failed: "+err.Error(), 1)
+			}
+		}
+		log.Println("Done!")
+		return
 	}
+	app.Run(os.Args)
 }
