ca8443938c4a2e46c5a5d6155c43c1eacdf7a6ad
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-12-31 13:21:41 -0200

    typo

--- config/config_test.go
+++ config/config_test.go
@@ -88,19 +88,19 @@ func TestValidadeMissingBinaryName(t *testing.T) {
 	assert := assert.New(t)
 
 	config := ProjectConfig{Repo: "asd/asd"}
-	assert.Error(config.validade())
+	assert.Error(config.validate())
 }
 
 func TestValidadeMissingRepo(t *testing.T) {
 	assert := assert.New(t)
 
 	config := ProjectConfig{BinaryName: "asd"}
-	assert.Error(config.validade())
+	assert.Error(config.validate())
 }
 
 func TestValidadeMinimalConfig(t *testing.T) {
 	assert := assert.New(t)
 
 	config := ProjectConfig{BinaryName: "asd", Repo: "asd/asd"}
-	assert.NoError(config.validade())
+	assert.NoError(config.validate())
 }
