6d884dde1bfb9b102f1e445b5f498b5b0071eb11
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-01-14 20:11:47 -0200

    fixes

--- pipeline/defaults/defaults_test.go
+++ pipeline/defaults/defaults_test.go
@@ -18,8 +18,8 @@ func TestFillBasicData(t *testing.T) {
 
 	assert.NoError(Pipe{}.Run(ctx))
 
-	assert.Equal("goreleaser/releaser", config.Repo)
-	assert.Equal("releaser", config.BinaryName)
+	assert.Equal("goreleaser/goreleaser", config.Release.Repo)
+	assert.Equal("goreleaser", config.Build.BinaryName)
 	assert.Equal("main.go", config.Build.Main)
 	assert.Equal("tar.gz", config.Archive.Format)
 	assert.Contains(config.Build.Goos, "darwin")
