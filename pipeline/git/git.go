4ca2bb210140ee5beb5ce82491d39d211ee4ed55
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-01-30 08:08:42 -0200

    erroring if version is invalid

--- pipeline/git/git.go
+++ pipeline/git/git.go
@@ -1,10 +1,20 @@
 package git
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/goreleaser/goreleaser/context"
 )
+
+// ErrInvalidVersionFormat is return when the version isnt in a valid format
+type ErrInvalidVersionFormat struct {
+	version string
+}
+
+func (e ErrInvalidVersionFormat) Error() string {
+	return e.version + " is not in a valid version format"
+}
 
 // Pipe for brew deployment
 type Pipe struct{}
@@ -36,5 +46,8 @@ func (Pipe) Run(ctx *context.Context) (err error) {
 	}
 	// removes usual `v` prefix
 	ctx.Version = strings.TrimPrefix(tag, "v")
+	if matches, err := regexp.MatchString("[0-9.]+", ctx.Version); !matches || err != nil {
+		return ErrInvalidVersionFormat{ctx.Version}
+	}
 	return
 }
