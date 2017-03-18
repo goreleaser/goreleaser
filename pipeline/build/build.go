35acdaee321ee526b7e38b6c7e5f7a41dae53889
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-03-18 20:40:26 -0300

    skip invalid builds
    closes https://github.com/goreleaser/goreleaser/issues/142

--- pipeline/build/build.go
+++ pipeline/build/build.go
@@ -26,6 +26,9 @@ func (Pipe) Run(ctx *context.Context) error {
 		for _, goarch := range ctx.Config.Build.Goarch {
 			goos := goos
 			goarch := goarch
+			if !valid(goos, goarch) {
+				continue
+			}
 			name, err := nameFor(ctx, goos, goarch)
 			if err != nil {
 				return err
@@ -74,4 +77,48 @@ func run(goos, goarch string, command []string) error {
 		return errors.New(string(out))
 	}
 	return nil
+}
+
+// list from https://golang.org/doc/install/source#environment
+var valids = []string{
+	"androidarm",
+	"darwin386",
+	"darwinamd64",
+	"darwinarm",
+	"darwinarm64",
+	"dragonflyamd64",
+	"freebsd386",
+	"freebsdamd64",
+	"freebsdarm",
+	"linux386",
+	"linuxamd64",
+	"linuxarm",
+	"linuxarm64",
+	"linuxppc64",
+	"linuxppc64le",
+	"linuxmips",
+	"linuxmipsle",
+	"linuxmips64",
+	"linuxmips64le",
+	"netbsd386",
+	"netbsdamd64",
+	"netbsdarm",
+	"openbsd386",
+	"openbsdamd64",
+	"openbsdarm",
+	"plan9386",
+	"plan9amd64",
+	"solarisamd64",
+	"windows386",
+	"windowsamd64",
+}
+
+func valid(goos, goarch string) bool {
+	var s = goos + goarch
+	for _, a := range valids {
+		if a == s {
+			return true
+		}
+	}
+	return false
 }
