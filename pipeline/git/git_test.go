03bae3d4daca82d8daf71c385cb62bb3002b3888
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-03-25 20:36:21 -0300

    added git pipeline tests

---
+++ pipeline/git/git_test.go
@@ -0,0 +1,21 @@
+package git
+
+import (
+	"testing"
+
+	"github.com/goreleaser/goreleaser/config"
+	"github.com/goreleaser/goreleaser/context"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidVersion(t *testing.T) {
+	assert := assert.New(t)
+
+	var ctx = &context.Context{
+		Config: config.Project{},
+	}
+	assert.NoError(Pipe{}.Run(ctx))
+	assert.NotEmpty(ctx.Git.CurrentTag)
+	assert.NotEmpty(ctx.Git.PreviousTag)
+	assert.NotEmpty(ctx.Git.Diff)
+}
