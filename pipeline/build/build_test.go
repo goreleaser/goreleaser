2885b24976ebd746c7fa593e2c510d2c8d6ec415
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-03-25 20:53:01 -0300

    improving code

--- pipeline/build/build_test.go
+++ pipeline/build/build_test.go
@@ -9,9 +9,3 @@ import (
 func TestDescription(t *testing.T) {
 	assert.NotEmpty(t, Pipe{}.Description())
 }
-
-func TestValid(t *testing.T) {
-	assert.True(t, valid("windows", "386"))
-	assert.True(t, valid("linux", "386"))
-	assert.False(t, valid("windows", "arm"))
-}
