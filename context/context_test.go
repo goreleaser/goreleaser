3a97034a34d8d84e4e468225d15fa19e08841f17
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-04-14 12:15:51 -0300

    added tests

---
+++ context/context_test.go
@@ -0,0 +1,34 @@
+package context
+
+import (
+	"testing"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/goreleaser/goreleaser/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMultipleArtifactAdds(t *testing.T) {
+	var assert = assert.New(t)
+	var list = []string{
+		"dist/a",
+		"dist/b",
+		"dist/c",
+		"dist/d",
+	}
+	var ctx = New(config.Project{
+		Dist: "dist",
+	})
+	var g errgroup.Group
+	for _, f := range list {
+		f := f
+		g.Go(func() error {
+			ctx.AddArtifact(f)
+			return nil
+		})
+	}
+	assert.NoError(g.Wait())
+	assert.Len(ctx.Artifacts, len(list))
+	assert.Contains(ctx.Artifacts, "a", "b", "c", "d")
+}
