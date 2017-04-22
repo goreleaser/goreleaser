5b36fbdd0c9d0bf964061559da739b375e55a52b
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-04-22 11:07:13 -0300

    added more tar tests

--- pipeline/archive/tar/tar_test.go
+++ pipeline/archive/tar/tar_test.go
@@ -20,8 +20,12 @@ func TestTarGzFile(t *testing.T) {
 	empty, err := os.Create(folder + "/empty.txt")
 	assert.NoError(err)
 
+	empty2, err := os.Create(folder + "/empty2.txt")
+	assert.NoError(err)
+
 	archive := New(file)
 	assert.NoError(archive.Add("empty.txt", empty.Name()))
 	assert.Error(archive.Add("dont.txt", empty.Name()+"_nope"))
 	assert.NoError(archive.Close())
+	assert.Error(archive.Add("empty2.txt", empty2.Name()))
 }
