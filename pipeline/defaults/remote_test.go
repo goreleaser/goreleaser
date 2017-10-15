529af6fe72a391207ab25ef6ca7676eaf4c45954
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-10-15 17:46:21 -0200

    refactor: improved git error handling
    Improved the error handling in git code,
    mostly in the defaults pipe. The idea is to
    output better error messages, hopefully avoiding
    confusion on "whats wrong".
    
    refs #356

--- pipeline/defaults/remote_test.go
+++ pipeline/defaults/remote_test.go
@@ -3,10 +3,16 @@ package defaults
 import (
 	"testing"
 
+	"github.com/goreleaser/goreleaser/internal/testlib"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRepoName(t *testing.T) {
+	_, back := testlib.Mktmp(t)
+	defer back()
+	testlib.GitInit(t)
+	testlib.GitRemoteAdd(t, "git@github.com:goreleaser/goreleaser.git")
 	repo, err := remoteRepo()
 	assert.NoError(t, err)
 	assert.Equal(t, "goreleaser/goreleaser", repo.String())
