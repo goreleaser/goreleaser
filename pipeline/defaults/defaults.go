31511b9b7791cad285bc9e24b2fe326374a7f442
Matt Stratton <matt.stratton@gmail.com>
2017-10-16 13:49:00 -0500

    feat: Add ability to specify release name
    TODO:  factor all the name stuff
     into a function like the others, so maybe
    setNameDefaults(ctx), etc.
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- pipeline/defaults/defaults.go
+++ pipeline/defaults/defaults.go
@@ -32,7 +32,7 @@ func (Pipe) Description() string {
 }
 
 // Run the pipe
-func (Pipe) Run(ctx *context.Context) error {
+func (Pipe) Run(ctx *context.Context) error { // nolint: gocyclo
 	ctx.Config.Dist = "dist"
 	if ctx.Config.Release.NameTemplate == "" {
 		ctx.Config.Release.NameTemplate = ReleaseNameTemplate
