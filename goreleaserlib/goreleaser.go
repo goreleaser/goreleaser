f3fcb489832091ba7120889935f15287fd767549
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-12-10 11:28:01 -0200

    test: added tests to effective config pipe
    Also renamed the pipe from finalconfig to effectiveconfig

--- goreleaserlib/goreleaser.go
+++ goreleaserlib/goreleaser.go
@@ -19,8 +19,8 @@ import (
 	"github.com/goreleaser/goreleaser/pipeline/defaults"
 	"github.com/goreleaser/goreleaser/pipeline/dist"
 	"github.com/goreleaser/goreleaser/pipeline/docker"
+	"github.com/goreleaser/goreleaser/pipeline/effectiveconfig"
 	"github.com/goreleaser/goreleaser/pipeline/env"
-	"github.com/goreleaser/goreleaser/pipeline/finalconfig"
 	"github.com/goreleaser/goreleaser/pipeline/fpm"
 	"github.com/goreleaser/goreleaser/pipeline/git"
 	"github.com/goreleaser/goreleaser/pipeline/release"
@@ -38,20 +38,20 @@ func init() {
 }
 
 var pipes = []pipeline.Piper{
-	defaults.Pipe{},    // load default configs
-	dist.Pipe{},        // ensure ./dist is clean
-	finalconfig.Pipe{}, // writes the actual config (with defaults et al set) to dist
-	git.Pipe{},         // get and validate git repo state
-	changelog.Pipe{},   // builds the release changelog
-	env.Pipe{},         // load and validate environment variables
-	build.Pipe{},       // build
-	archive.Pipe{},     // archive (tar.gz, zip, etc)
-	fpm.Pipe{},         // archive via fpm (deb, rpm, etc)
-	snapcraft.Pipe{},   // archive via snapcraft (snap)
-	checksums.Pipe{},   // checksums of the files
-	docker.Pipe{},      // create and push docker images
-	release.Pipe{},     // release to github
-	brew.Pipe{},        // push to brew tap
+	defaults.Pipe{},        // load default configs
+	dist.Pipe{},            // ensure ./dist is clean
+	effectiveconfig.Pipe{}, // writes the actual config (with defaults et al set) to dist
+	git.Pipe{},             // get and validate git repo state
+	changelog.Pipe{},       // builds the release changelog
+	env.Pipe{},             // load and validate environment variables
+	build.Pipe{},           // build
+	archive.Pipe{},         // archive (tar.gz, zip, etc)
+	fpm.Pipe{},             // archive via fpm (deb, rpm, etc)
+	snapcraft.Pipe{},       // archive via snapcraft (snap)
+	checksums.Pipe{},       // checksums of the files
+	docker.Pipe{},          // create and push docker images
+	release.Pipe{},         // release to github
+	brew.Pipe{},            // push to brew tap
 }
 
 // Flags interface represents an extractor of cli flags
