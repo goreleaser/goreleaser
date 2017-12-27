9b0441cbe481eb80bacd76e391d63d331c4fff6c
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-12-26 22:51:28 -0200

    docs: godoc of template pkg

--- internal/template/template.go
+++ internal/template/template.go
@@ -1,3 +1,5 @@
+// Package template contains the code used to template names of goreleaser's
+// packages and archives.
 package template
 
 import (
@@ -8,6 +10,7 @@ import (
 	"github.com/goreleaser/goreleaser/internal/artifact"
 )
 
+// Fields contains all accepted fields in the template string
 type Fields struct {
 	Version     string
 	Tag         string
@@ -19,6 +22,7 @@ type Fields struct {
 	Binary      string
 }
 
+// NewFields returns a Fields instances filled with the data provided
 func NewFields(ctx *context.Context, a artifact.Artifact, replacements map[string]string) Fields {
 	return Fields{
 		Env:         ctx.Env,
@@ -32,6 +36,8 @@ func NewFields(ctx *context.Context, a artifact.Artifact, replacements map[strin
 	}
 }
 
+// Apply applies the given fields to the given template and returns the
+// evaluation and any error that might occur.
 func Apply(tmpl string, fields Fields) (string, error) {
 	t, err := gotemplate.New(tmpl).Parse(tmpl)
 	if err != nil {
