8e1c25ed0c82251fafd8047534ac165c9577be2b
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-02-03 00:06:48 -0200

    feat: load github token from file

--- pipeline/env/env_test.go
+++ pipeline/env/env_test.go
@@ -2,6 +2,7 @@ package env
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 
@@ -13,6 +14,24 @@ import (
 
 func TestDescription(t *testing.T) {
 	assert.NotEmpty(t, Pipe{}.String())
+}
+
+func TestDefault(t *testing.T) {
+	t.Run("empty config", func(tt *testing.T) {
+		ctx := context.New(config.Project{})
+		assert.NoError(t, Pipe{}.Default(ctx))
+		assert.Equal(t, "~/.config/goreleaser/github_token", ctx.Config.EnvFiles.GitHubToken)
+	})
+	t.Run("custom config config", func(tt *testing.T) {
+		cfg := "what"
+		ctx := context.New(config.Project{
+			EnvFiles: config.EnvFiles{
+				GitHubToken: cfg,
+			},
+		})
+		assert.NoError(t, Pipe{}.Default(ctx))
+		assert.Equal(t, cfg, ctx.Config.EnvFiles.GitHubToken)
+	})
 }
 
 func TestValidEnv(t *testing.T) {
@@ -35,12 +54,37 @@ func TestInvalidEnv(t *testing.T) {
 	assert.Error(t, Pipe{}.Run(ctx))
 }
 
-type flags struct {
-	Validate, Publish, Snapshot bool
+func TestEmptyFileEnv(t *testing.T) {
+	assert.NoError(t, os.Unsetenv("GITHUB_TOKEN"))
+	var ctx = &context.Context{
+		Config:   config.Project{},
+		Validate: true,
+		Publish:  true,
+	}
+	assert.Error(t, Pipe{}.Run(ctx))
+}
+
+func TestEmptyEnvFile(t *testing.T) {
+	assert.NoError(t, os.Unsetenv("GITHUB_TOKEN"))
+	f, err := ioutil.TempFile("", "token")
+	assert.NoError(t, err)
+	assert.NoError(t, os.Chmod(f.Name(), 0377))
+	var ctx = &context.Context{
+		Config: config.Project{
+			EnvFiles: config.EnvFiles{
+				GitHubToken: f.Name(),
+			},
+		},
+		Validate: true,
+		Publish:  true,
+	}
+	assert.EqualError(t, Pipe{}.Run(ctx), fmt.Sprintf("failed to load github token: open %s: permission denied", f.Name()))
 }
 
 func TestInvalidEnvChecksSkipped(t *testing.T) {
-	for _, flag := range []flags{
+	for _, flag := range []struct {
+		Validate, Publish, Snapshot bool
+	}{
 		{
 			Validate: false,
 			Publish:  true,
@@ -62,4 +106,35 @@ func TestInvalidEnvChecksSkipped(t *testing.T) {
 			testlib.AssertSkipped(t, Pipe{}.Run(ctx))
 		})
 	}
+}
+
+func TestLoadEnv(t *testing.T) {
+	t.Run("env exists", func(tt *testing.T) {
+		var env = "SUPER_SECRET_ENV"
+		assert.NoError(tt, os.Setenv(env, "1"))
+		v, err := loadEnv(env, "nope")
+		assert.NoError(tt, err)
+		assert.Equal(tt, "1", v)
+	})
+	t.Run("env file exists", func(tt *testing.T) {
+		var env = "SUPER_SECRET_ENV_NOPE"
+		assert.NoError(tt, os.Unsetenv(env))
+		f, err := ioutil.TempFile("", "token")
+		assert.NoError(t, err)
+		fmt.Fprintf(f, "123")
+		v, err := loadEnv(env, f.Name())
+		assert.NoError(tt, err)
+		assert.Equal(tt, "123", v)
+	})
+	t.Run("env file is not readable", func(tt *testing.T) {
+		var env = "SUPER_SECRET_ENV_NOPE"
+		assert.NoError(tt, os.Unsetenv(env))
+		f, err := ioutil.TempFile("", "token")
+		assert.NoError(t, err)
+		fmt.Fprintf(f, "123")
+		os.Chmod(f.Name(), 0377)
+		v, err := loadEnv(env, f.Name())
+		assert.EqualError(tt, err, fmt.Sprintf("open %s: permission denied", f.Name()))
+		assert.Equal(tt, "", v)
+	})
 }
