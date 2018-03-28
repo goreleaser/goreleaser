3c1f856ddf9ea9452ac6fdb5a0e3dc3eaaee38e3
Dominik Schulz <dominik.schulz@justwatch.com>
2018-03-28 16:14:27 +0200

    fix: improve homebrew formula
    This commit improves the homebrew formula pipeline step to support
    more customizations.

--- config/config.go
+++ config/config.go
@@ -34,20 +34,23 @@ func (r Repo) String() string {
 
 // Homebrew contains the brew section
 type Homebrew struct {
-	Name             string       `yaml:",omitempty"`
-	GitHub           Repo         `yaml:",omitempty"`
-	CommitAuthor     CommitAuthor `yaml:"commit_author,omitempty"`
-	Folder           string       `yaml:",omitempty"`
-	Caveats          string       `yaml:",omitempty"`
-	Plist            string       `yaml:",omitempty"`
-	Install          string       `yaml:",omitempty"`
-	Dependencies     []string     `yaml:",omitempty"`
-	Test             string       `yaml:",omitempty"`
-	Conflicts        []string     `yaml:",omitempty"`
-	Description      string       `yaml:",omitempty"`
-	Homepage         string       `yaml:",omitempty"`
-	SkipUpload       bool         `yaml:"skip_upload,omitempty"`
-	DownloadStrategy string       `yaml:"download_strategy,omitempty"`
+	Name              string       `yaml:",omitempty"`
+	GitHub            Repo         `yaml:",omitempty"`
+	CommitAuthor      CommitAuthor `yaml:"commit_author,omitempty"`
+	Folder            string       `yaml:",omitempty"`
+	Caveats           string       `yaml:",omitempty"`
+	Plist             string       `yaml:",omitempty"`
+	Install           string       `yaml:",omitempty"`
+	Dependencies      []string     `yaml:",omitempty"`
+	BuildDependencies []string     `yaml:"build_dependencies,omitempty"`
+	Test              string       `yaml:",omitempty"`
+	Special           string       `yaml:",omitempty"`
+	Conflicts         []string     `yaml:",omitempty"`
+	Description       string       `yaml:",omitempty"`
+	Homepage          string       `yaml:",omitempty"`
+	SkipUpload        bool         `yaml:"skip_upload,omitempty"`
+	DownloadStrategy  string       `yaml:"download_strategy,omitempty"`
+	SourceTarball     string       `yaml:"-"`
 }
 
 // Scoop contains the scoop.sh section
