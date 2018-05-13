87926dacbdf0304a99819575476897a3c12c4551
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-05-13 14:44:49 -0300

    test: cover default method

--- pipeline/s3/s3_test.go
+++ pipeline/s3/s3_test.go
@@ -15,3 +15,31 @@ func TestDescription(t *testing.T) {
 func TestNoS3(t *testing.T) {
 	assert.NoError(t, Pipe{}.Run(context.New(config.Project{})))
 }
+
+func TestDefaultsNoS3(t *testing.T) {
+	var assert = assert.New(t)
+	var ctx = context.New(config.Project{
+		S3: []config.S3{
+			config.S3{},
+		},
+	})
+	assert.NoError(Pipe{}.Default(ctx))
+	assert.Equal([]config.S3{config.S3{}}, ctx.Config.S3)
+}
+
+func TestDefaults(t *testing.T) {
+	var assert = assert.New(t)
+	var ctx = context.New(config.Project{
+		S3: []config.S3{
+			{
+				Bucket: "foo",
+			},
+		},
+	})
+	assert.NoError(Pipe{}.Default(ctx))
+	assert.Equal([]config.S3{config.S3{
+		Bucket: "foo",
+		Region: "us-east-1",
+		Folder: "{{ .ProjectName }}/{{ .Tag }}",
+	}}, ctx.Config.S3)
+}
