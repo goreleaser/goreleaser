90656f8550bdcade80f8b140de1ba2e07bde1f5f
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-06-20 09:46:42 -0300

    fix: breaking: remove the build dependencies from the brew pipe
    as for now, it just doesnt work, and maybe it doesnt make sense
    to have it anyway

--- pipeline/brew/template.go
+++ pipeline/brew/template.go
@@ -3,46 +3,35 @@ package brew
 import "github.com/goreleaser/goreleaser/config"
 
 type templateData struct {
-	Name              string
-	Desc              string
-	Homepage          string
-	DownloadURL       string
-	Repo              config.Repo // FIXME: will not work for anything but github right now.
-	Tag               string
-	Version           string
-	Caveats           []string
-	File              string
-	SHA256            string
-	Plist             string
-	DownloadStrategy  string
-	Install           []string
-	Dependencies      []string
-	BuildDependencies []string
-	Conflicts         []string
-	Tests             []string
+	Name             string
+	Desc             string
+	Homepage         string
+	DownloadURL      string
+	Repo             config.Repo // FIXME: will not work for anything but github right now.
+	Tag              string
+	Version          string
+	Caveats          []string
+	File             string
+	SHA256           string
+	Plist            string
+	DownloadStrategy string
+	Install          []string
+	Dependencies     []string
+	Conflicts        []string
+	Tests            []string
 }
 
 const formulaTemplate = `class {{ .Name }} < Formula
   desc "{{ .Desc }}"
   homepage "{{ .Homepage }}"
-  {{ if .BuildDependencies -}}
-  url "{{ .DownloadURL }}/{{ .Repo.Owner }}/{{ .Repo.Name }}/archive/{{ .Tag }}.tar.gz"
-  head "https://github.com/{{ .Repo.Owner }}/{{ .Repo.Name }}.git"
-  {{- else -}}
   url "{{ .DownloadURL }}/{{ .Repo.Owner }}/{{ .Repo.Name }}/releases/download/{{ .Tag }}/{{ .File }}"
   {{- if .DownloadStrategy }}, :using => {{ .DownloadStrategy }}{{- end }}
-  {{- end }}
   version "{{ .Version }}"
   sha256 "{{ .SHA256 }}"
 
   {{- with .Dependencies }}
   {{ range $index, $element := . }}
   depends_on "{{ . }}"
-  {{- end }}
-  {{- end -}}
-  {{- with .BuildDependencies -}}
-  {{ range $index, $element := . }}
-  depends_on "{{ . }}" => :build
   {{- end }}
   {{- end -}}
 
