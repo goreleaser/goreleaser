831cf0600c619d82afb537e5a5d0bce8c44df8a8
Igor German <iggerman@yandex.ru>
2018-09-04 17:10:41 +0300

    fix: entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -n $DOCKER_USERNAME ] && [ -n $DOCKER_PASSWORD ]; then
+if [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
     echo "Login to the docker..."
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 fi
