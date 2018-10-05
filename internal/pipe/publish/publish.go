6d7d13a3f805a114c90483eb30181f11a9035855
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-10-05 09:22:53 -0300

    refactor: added publish pipe

---
+++ internal/pipe/publish/publish.go
@@ -0,0 +1,40 @@
+// Package publish contains the publishing pipe.
+package publish
+
+import (
+	"fmt"
+
+	"github.com/goreleaser/goreleaser/internal/pipe"
+	"github.com/goreleaser/goreleaser/pkg/context"
+	"github.com/pkg/errors"
+)
+
+// Pipe that publishes artifacts
+type Pipe struct{}
+
+func (Pipe) String() string {
+	return "publishing artifacts"
+}
+
+// Publisher should be implemented by pipes that want to publish artifacts
+type Publisher interface {
+	fmt.Stringer
+
+	// Default sets the configuration defaults
+	Publish(ctx *context.Context) error
+}
+
+var publishers = []Publisher{}
+
+// Run the pipe
+func (Pipe) Run(ctx *context.Context) error {
+	if ctx.SkipPublish {
+		return pipe.ErrSkipPublishEnabled
+	}
+	for _, publisher := range publishers {
+		if err := publisher.Publish(ctx); err != nil {
+			return errors.Wrapf(err, "failed to publish artifacts for %s", publisher.String())
+		}
+	}
+	return nil
+}
---
+++ internal/pipe/publish/publish_test.go
@@ -0,0 +1,22 @@
+package publish
+
+import (
+	"testing"
+
+	"github.com/goreleaser/goreleaser/internal/pipe"
+	"github.com/goreleaser/goreleaser/pkg/config"
+	"github.com/goreleaser/goreleaser/pkg/context"
+	"github.com/stretchr/testify/require"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDescription(t *testing.T) {
+	assert.NotEmpty(t, Pipe{}.String())
+}
+
+func TestPublishDisable(t *testing.T) {
+	var ctx = context.New(config.Project{})
+	ctx.SkipPublish = true
+	require.EqualError(t, Pipe{}.Run(ctx), pipe.ErrSkipPublishEnabled.Error())
+}
