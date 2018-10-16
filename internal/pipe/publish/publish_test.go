0b04e454fc0d59e113b38633b9951b0dd85f3463
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-10-16 20:50:18 -0300

    test: fix

--- internal/pipe/publish/publish_test.go
+++ internal/pipe/publish/publish_test.go
@@ -7,12 +7,10 @@ import (
 	"github.com/goreleaser/goreleaser/pkg/config"
 	"github.com/goreleaser/goreleaser/pkg/context"
 	"github.com/stretchr/testify/require"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestDescription(t *testing.T) {
-	assert.NotEmpty(t, Pipe{}.String())
+	require.NotEmpty(t, Pipe{}.String())
 }
 
 func TestPublishDisable(t *testing.T) {
@@ -23,5 +21,6 @@ func TestPublishDisable(t *testing.T) {
 
 func TestPublish(t *testing.T) {
 	var ctx = context.New(config.Project{})
+	ctx.Config.Release.Disable = true
 	require.NoError(t, Pipe{}.Run(ctx))
 }
