9b0a75d24de7bf3323b2caa9511e41e2b08895a2
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-10-26 18:27:17 -0300

    test: disable colors on tests

--- internal/deprecate/deprecate_test.go
+++ internal/deprecate/deprecate_test.go
@@ -4,10 +4,10 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/cli"
+	"github.com/fatih/color"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNotice(t *testing.T) {
@@ -17,6 +17,7 @@ func TestNotice(t *testing.T) {
 	log.Info("first")
 	Notice("foo.bar.whatever")
 	log.Info("last")
+	color.NoColor = true
 
 	assert.Contains(t, out.String(), "   • first")
 	assert.Contains(t, out.String(), "      • DEPRECATED: `foo.bar.whatever` should not be used anymore, check https://goreleaser.com/deprecations#foo-bar-whatever for more info.")
