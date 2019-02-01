413a61156a1cfaddeca877891f10df927e859741
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2019-02-02 02:53:16 +0300

    internal/pipe/brew: fix strings.HasPrefix args order
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- internal/pipe/brew/brew.go
+++ internal/pipe/brew/brew.go
@@ -150,7 +150,7 @@ func ghFormulaPath(folder, filename string) string {
 
 func getFormat(ctx *context.Context) string {
 	for _, override := range ctx.Config.Archive.FormatOverrides {
-		if strings.HasPrefix("darwin", override.Goos) {
+		if strings.HasPrefix(override.Goos, "darwin") {
 			return override.Format
 		}
 	}
