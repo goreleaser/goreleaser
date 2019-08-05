2344cbed9d900a6042aec2a4b0c5550ac4f28b04
Tom Manville <tom@kasten.io>
2019-08-04 18:54:55 -0700

    Separate implementation for parallel vs. serial

--- internal/semerrgroup/sem.go
+++ internal/semerrgroup/sem.go
@@ -2,26 +2,40 @@
 // size, so you can control the number of tasks being executed simultaneously.
 package semerrgroup
 
-import "golang.org/x/sync/errgroup"
+import (
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
 
 // Group is the Semphore ErrorGroup itself
-type Group struct {
-	ch chan bool
-	g  errgroup.Group
+type Group interface {
+	Go(func() error)
+	Wait() error
 }
 
 // New returns a new Group of a given size.
-func New(size int) *Group {
-	return &Group{
+func New(size int) Group {
+	if size == 1 {
+		return &serialGroup{}
+	}
+	return &parallelGroup{
 		ch: make(chan bool, size),
 		g:  errgroup.Group{},
 	}
 }
 
+var _ Group = &parallelGroup{}
+
+type parallelGroup struct {
+	ch chan bool
+	g  errgroup.Group
+}
+
 // Go execs one function respecting the group and semaphore.
-func (s *Group) Go(fn func() error) {
-	s.ch <- true
+func (s *parallelGroup) Go(fn func() error) {
 	s.g.Go(func() error {
+		s.ch <- true
 		defer func() {
 			<-s.ch
 		}()
@@ -30,6 +44,29 @@ func (s *Group) Go(fn func() error) {
 }
 
 // Wait waits for the group to complete and return an error if any.
-func (s *Group) Wait() error {
+func (s *parallelGroup) Wait() error {
 	return s.g.Wait()
+}
+
+var _ Group = &serialGroup{}
+
+type serialGroup struct {
+	lock sync.Mutex
+	err  error
+}
+
+// Go execs one function at a time.
+func (s *serialGroup) Go(fn func() error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	if err := fn(); err != nil && s.err == nil {
+		s.err = err
+	}
+}
+
+// Wait waits for Go to complete and returns the first error encountered.
+func (s *serialGroup) Wait() error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	return s.err
 }
