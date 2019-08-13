de5676e83310ff83d3f32d79fd46510533a3e1d6
Grachev Mikhail <work@mgrachev.com>
2019-08-13 19:37:11 +0300

    feat: support arm for linuxbrew (#1113)

--- internal/pipe/brew/template.go
+++ internal/pipe/brew/template.go
@@ -16,6 +16,8 @@ type templateData struct {
 	CustomBlock      []string
 	MacOS            downloadable
 	Linux            downloadable
+	Arm              downloadable
+	Arm64            downloadable
 }
 
 type downloadable struct {
@@ -36,15 +38,34 @@ class {{ .Name }} < Formula
   if OS.mac?
     {{- if .MacOS.DownloadURL }}
     url "{{ .MacOS.DownloadURL }}"
-	{{- if .DownloadStrategy }}, :using => {{ .DownloadStrategy }}{{- end }}
+    {{- if .DownloadStrategy }}, :using => {{ .DownloadStrategy }}{{- end }}
     sha256 "{{ .MacOS.SHA256 }}"
     {{- end }}
   elsif OS.linux?
     {{- if .Linux.DownloadURL }}
-    url "{{ .Linux.DownloadURL }}"
-	{{- if .DownloadStrategy }}, :using => {{ .DownloadStrategy }}{{- end }}
-    sha256 "{{ .Linux.SHA256 }}"
+    if Hardware::CPU.intel?
+      url "{{ .Linux.DownloadURL }}"
+      {{- if .DownloadStrategy }}, :using => {{ .DownloadStrategy }}{{- end }}
+      sha256 "{{ .Linux.SHA256 }}"
+    end
     {{- end }}
+	{{- if or .Arm.DownloadURL .Arm64.DownloadURL }}
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        {{- if .Arm64.DownloadURL }}
+        url "{{ .Arm64.DownloadURL }}"
+        {{- if .DownloadStrategy }}, :using => {{ .DownloadStrategy }}{{- end }}
+        sha256 "{{ .Arm64.SHA256 }}"
+        {{- end }}
+      else
+        {{- if .Arm.DownloadURL }}
+        url "{{ .Arm.DownloadURL }}"
+        {{- if .DownloadStrategy }}, :using => {{ .DownloadStrategy }}{{- end }}
+        sha256 "{{ .Arm.SHA256 }}"
+        {{- end }}
+      end
+    end
+	{{- end }}
   end
 
   {{- with .CustomBlock }}
