fc1aa7abbd8b7a9a80b572c9c61e5769c3c90c02
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-09-12 15:56:49 -0300

    fix: go mod init
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- main_test.go
+++ main_test.go
@@ -161,6 +161,7 @@ func setup(t *testing.T) (current string, back func()) {
 	assert.NoError(t, os.Chdir(folder))
 	createGoreleaserYaml(t)
 	createMainGo(t)
+	goModInit(t)
 	testlib.GitInit(t)
 	testlib.GitAdd(t)
 	testlib.GitCommit(t, "asdf")
@@ -181,6 +182,13 @@ func createFile(t *testing.T, filename, contents string) {
 
 func createMainGo(t *testing.T) {
 	createFile(t, "main.go", "package main\nfunc main() {println(0)}")
+}
+
+func goModInit(t *testing.T) {
+	createFile(t, "go.mod", `module foo
+
+go 1.13
+`)
 }
 
 func createGoreleaserYaml(t *testing.T) {
