8cb910cbd08eabbea889595c1bdd8897e4e39936
Alexander Baryshnikov <dev@baryshnikov.net>
2020-02-21 22:09:01 +0800

    add basic tests

--- internal/pipe/upload/upload_test.go
+++ internal/pipe/upload/upload_test.go
@@ -282,6 +282,81 @@ func TestRunPipe_ModeBinary_CustomArtifactName(t *testing.T) {
 	assert.NoError(t, Pipe{}.Publish(ctx))
 }
 
+func TestRunPipe_FilterByName(t *testing.T) {
+	setup()
+	defer teardown()
+
+	folder, err := ioutil.TempDir("", "archivetest")
+	assert.NoError(t, err)
+	var dist = filepath.Join(folder, "dist")
+	assert.NoError(t, os.Mkdir(dist, 0755))
+	assert.NoError(t, os.Mkdir(filepath.Join(dist, "mybin"), 0755))
+	var binPath = filepath.Join(dist, "mybin", "mybin")
+	d1 := []byte("hello\ngo\n")
+	err = ioutil.WriteFile(binPath, d1, 0666)
+	assert.NoError(t, err)
+
+	// Dummy http server
+	mux.HandleFunc("/example-repo-local/mybin/darwin/amd64/mybin;deb.distribution=xenial", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPut)
+		testHeader(t, r, "Content-Length", "9")
+		// Basic auth of user "deployuser" with secret "deployuser-secret"
+		testHeader(t, r, "Authorization", "Basic ZGVwbG95dXNlcjpkZXBsb3l1c2VyLXNlY3JldA==")
+
+		w.Header().Set("Location", "/production-repo-remote/mybin/linux/amd64/mybin;deb.distribution=xenial")
+		w.WriteHeader(http.StatusCreated)
+	})
+	mux.HandleFunc("/example-repo-local/mybin/linux/amd64/mybin;deb.distribution=xenial", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPut)
+		testHeader(t, r, "Content-Length", "9")
+		// Basic auth of user "deployuser" with secret "deployuser-secret"
+		testHeader(t, r, "Authorization", "Basic ZGVwbG95dXNlcjpkZXBsb3l1c2VyLXNlY3JldA==")
+
+		w.Header().Set("Location", "/example-repo-local/mybin/linux/amd64/mybin;deb.distribution=xenial")
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	var ctx = context.New(config.Project{
+		ProjectName: "mybin",
+		Dist:        dist,
+		Uploads: []config.Upload{
+			{
+				Method:             h.MethodPut,
+				Name:               "production-us",
+				Mode:               "binary",
+				Target:             fmt.Sprintf("%s/example-repo-local/{{ .ProjectName }}/{{ .Os }}/{{ .Arch }}{{ if .Arm }}v{{ .Arm }}{{ end }}/{{ .ArtifactName }};deb.distribution=xenial", server.URL),
+				Username:           "deployuser",
+				CustomArtifactName: true,
+				Pattern:            "^mybin$",
+			},
+		},
+		Archives: []config.Archive{
+			{},
+		},
+	})
+	ctx.Env = map[string]string{
+		"UPLOAD_PRODUCTION-US_SECRET": "deployuser-secret",
+	}
+	for _, goos := range []string{"linux", "darwin"} {
+		ctx.Artifacts.Add(&artifact.Artifact{
+			Name:   "mybin",
+			Path:   binPath,
+			Goarch: "amd64",
+			Goos:   goos,
+			Type:   artifact.UploadableBinary,
+		})
+		ctx.Artifacts.Add(&artifact.Artifact{
+			Name:   "nothing",
+			Path:   binPath,
+			Goarch: "amd64",
+			Goos:   goos,
+			Type:   artifact.UploadableBinary,
+		})
+	}
+
+	assert.NoError(t, Pipe{}.Publish(ctx))
+}
+
 func TestRunPipe_ModeArchive_CustomArtifactName(t *testing.T) {
 	setup()
 	defer teardown()
