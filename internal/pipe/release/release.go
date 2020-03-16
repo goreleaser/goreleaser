ce5e3f4354afd523a492aa6a9f43a4039bfdac99
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-03-16 12:54:50 -0300

    fix: logs
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- internal/pipe/release/release.go
+++ internal/pipe/release/release.go
@@ -176,10 +176,10 @@ func upload(ctx *context.Context, client client.Client, releaseID string, artifa
 		defer file.Close() // nolint: errcheck
 		log.WithField("file", file.Name()).WithField("name", artifact.Name).Info("uploading to release")
 		if err := client.Upload(ctx, releaseID, artifact, file); err != nil {
-			log.WithFields(log.Fields{
-				"try":      try,
-				"artifact": artifact.Name,
-			}).Warnf("failed to upload artifact, will retry")
+			log.WithField("try", try).
+				WithField("artifact", artifact.Name).
+				WithError(err).
+				Warnf("failed to upload artifact, will retry")
 			return err
 		}
 		return nil
