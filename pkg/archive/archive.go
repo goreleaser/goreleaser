e4e9b9395a292936194d4b6a8e91b84f4cf18396
John Taylor <johntaylor@gmail.com>
2020-04-12 17:52:38 +0000

    remove .xz feature

--- pkg/archive/archive.go
+++ pkg/archive/archive.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/goreleaser/goreleaser/pkg/archive/gzip"
 	"github.com/goreleaser/goreleaser/pkg/archive/targz"
-	"github.com/goreleaser/goreleaser/pkg/archive/xz"
 	"github.com/goreleaser/goreleaser/pkg/archive/tarxz"
 	"github.com/goreleaser/goreleaser/pkg/archive/zip"
 )
@@ -28,9 +27,6 @@ func New(file *os.File) Archive {
 	}
 	if strings.HasSuffix(file.Name(), ".tar.xz") {
 		return tarxz.New(file)
-	}
-	if strings.HasSuffix(file.Name(), ".xz") {
-		return xz.New(file)
 	}
 	if strings.HasSuffix(file.Name(), ".zip") {
 		return zip.New(file)
