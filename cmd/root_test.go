d73a41db9e0ea894fd83a228727378f0b22135b5
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-04-26 20:50:19 -0300

    feat: refactor cli
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,13 +1,23 @@
 package cmd
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestRootCmd(t *testing.T) {
-	var cmd = NewRootCmd("foo")
-
+func TestRootCmdHelp(t *testing.T) {
+	var cmd = NewRootCmd("foo").cmd
+	cmd.SetArgs([]string{"-h"})
 	require.NoError(t, cmd.Execute())
+}
+
+func TestRootCmdVersion(t *testing.T) {
+	var b bytes.Buffer
+	var cmd = NewRootCmd("foo").cmd
+	cmd.SetOut(&b)
+	cmd.SetArgs([]string{"-v"})
+	require.NoError(t, cmd.Execute())
+	require.Equal(t, "goreleaser version foo\n", b.String())
 }
