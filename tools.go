955f7eee76c5bee8f73ba394262ca5af3af64a2d
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2020-05-10 21:34:55 +0200

    feat: Use tools.go paradigm for external dependencies (#1495)
    Co-authored-by: CrazyMax <crazy-max@users.noreply.github.com>

---
+++ tools.go
@@ -0,0 +1,10 @@
+// +build tools
+
+package tools
+
+import (
+	// Import golangci-lint
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	// Import misspell
+	_ "github.com/client9/misspell/cmd/misspell"
+)
