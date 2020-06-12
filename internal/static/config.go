3f8468597a66303d224467032cc2d710d8c83e6b
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-06-12 09:50:16 -0300

    fix: removed unused code
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- internal/static/config.go
+++ internal/static/config.go
@@ -1,3 +1,6 @@
+// Package static contains static "text" files, just because embedding real
+// static files would be kind of an overengineering right now, so it's just
+// strings in go code really.
 package static
 
 // ExampleConfig is the config used within goreleaser init.
