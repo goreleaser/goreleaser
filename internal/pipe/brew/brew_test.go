78b0097dc26d86543934eb842ca0cf012ca2d2bd
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2021-01-21 18:01:33 -0300

    fix: brew default install when using templates in the binary name (#2030)
    * fix: brew default install when using templates in the binary name
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * test: fix
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * test: fix
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: remove unused func
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    * fix: remove unused func
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- internal/pipe/brew/brew_test.go
+++ internal/pipe/brew/brew_test.go
@@ -840,36 +840,6 @@ func TestRunTokenTypeNotImplementedForBrew(t *testing.T) {
 	require.Equal(t, ErrTokenTypeNotImplementedForBrew{TokenType: "gitea"}, doRun(ctx, ctx.Config.Brews[0], client))
 }
 
-func TestDefaultBinInstallUniqueness(t *testing.T) {
-	testlib.Mktmp(t)
-
-	var ctx = &context.Context{
-		TokenType: context.TokenTypeGitHub,
-		Config: config.Project{
-			ProjectName: "myproject",
-			Brews: []config.Homebrew{
-				{},
-			},
-			Builds: []config.Build{
-				{
-					ID:     "macos",
-					Binary: "unique",
-					Goos:   []string{"darwin"},
-					Goarch: []string{"amd64"},
-				},
-				{
-					ID:     "macos-cgo",
-					Binary: "unique",
-					Goos:   []string{"darwin"},
-					Goarch: []string{"amd64"},
-				},
-			},
-		},
-	}
-	require.NoError(t, Pipe{}.Default(ctx))
-	require.Equal(t, `bin.install "unique"`, ctx.Config.Brews[0].Install)
-}
-
 func TestDefault(t *testing.T) {
 	testlib.Mktmp(t)
 
@@ -906,7 +876,7 @@ func TestDefault(t *testing.T) {
 	require.Equal(t, ctx.Config.ProjectName, ctx.Config.Brews[0].Name)
 	require.NotEmpty(t, ctx.Config.Brews[0].CommitAuthor.Name)
 	require.NotEmpty(t, ctx.Config.Brews[0].CommitAuthor.Email)
-	require.Equal(t, `bin.install "foo"`, ctx.Config.Brews[0].Install)
+	require.Equal(t, `bin.install "myproject"`, ctx.Config.Brews[0].Install)
 }
 
 func TestGHFolder(t *testing.T) {
