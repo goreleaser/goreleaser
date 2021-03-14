7b421ac0aaf172194f38a997bd4f87461aba3eff
Peer Xu <pppeerxu@gmail.com>
2021-03-14 20:20:03 +0800

    feat: add layout field to snapcraft pipe

--- internal/pipe/snapcraft/snapcraft_test.go
+++ internal/pipe/snapcraft/snapcraft_test.go
@@ -153,6 +153,12 @@ func TestRunPipeMetadata(t *testing.T) {
 				NameTemplate: "foo_{{.Arch}}",
 				Summary:      "test summary",
 				Description:  "test description",
+				Layout: map[string]config.SnapcraftLayoutMetadata{
+					"/etc/testprojectname":             {Bind: "$SNAP_DATA/etc"},
+					"/etc/testprojectname/config.yaml": {BindFile: "$SNAP_DATA/etc/config.yaml"},
+					"/etc/testprojectname/symlink":     {Symlink: "$SNAP_DATA/etc/symlink"},
+					"/etc/testprojectname/tmp":         {Type: "tmpfs"},
+				},
 				Apps: map[string]config.SnapcraftAppMetadata{
 					"foo": {
 						Plugs:            []string{"home", "network", "personal-files"},
@@ -187,6 +193,10 @@ func TestRunPipeMetadata(t *testing.T) {
 	require.Equal(t, "foo --foo --bar", metadata.Apps["foo"].Command)
 	require.Equal(t, map[interface{}]interface{}{"read": []interface{}{"$HOME/test"}}, metadata.Plugs["personal-files"])
 	require.Equal(t, "always", metadata.Apps["foo"].RestartCondition)
+	require.Equal(t, "$SNAP_DATA/etc", metadata.Layout["/etc/testprojectname"].Bind)
+	require.Equal(t, "$SNAP_DATA/etc/config.yaml", metadata.Layout["/etc/testprojectname/config.yaml"].BindFile)
+	require.Equal(t, "$SNAP_DATA/etc/symlink", metadata.Layout["/etc/testprojectname/symlink"].Symlink)
+	require.Equal(t, "tmpfs", metadata.Layout["/etc/testprojectname/tmp"].Type)
 }
 
 func TestNoSnapcraftInPath(t *testing.T) {
