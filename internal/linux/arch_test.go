12c7d38eb9c1fcb4ee45320d5dc6e1a4eaf97d2f
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2021-03-20 15:16:26 -0300

    fix: properly translate mips arch (#2126)
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- internal/linux/arch_test.go
+++ internal/linux/arch_test.go
@@ -9,15 +9,19 @@ import (
 
 func TestArch(t *testing.T) {
 	for from, to := range map[string]string{
-		"linuxamd64":   "amd64",
-		"linux386":     "i386",
-		"linuxarm64":   "arm64",
-		"linuxarm5":    "armel",
-		"linuxarm6":    "armhf",
-		"linuxarm7":    "armhf",
-		"linuxppc64":   "ppc64",
-		"linuxppc64le": "ppc64le",
-		"linuxwhat":    "what",
+		"linuxamd64":             "amd64",
+		"linux386":               "i386",
+		"linuxarm64":             "arm64",
+		"linuxarm5":              "armel",
+		"linuxarm6":              "armhf",
+		"linuxarm7":              "armhf",
+		"linuxppc64":             "ppc64",
+		"linuxppc64le":           "ppc64le",
+		"linuxwhat":              "what",
+		"linuxmips64lesoftfloat": "mips64el",
+		"linuxmipslehardfloat":   "mipsel",
+		"linuxmipssoftfloat":     "mips",
+		"linuxmips64hardfloat":   "mips64",
 	} {
 		t.Run(fmt.Sprintf("%s to %s", from, to), func(t *testing.T) {
 			require.Equal(t, to, Arch(from))
