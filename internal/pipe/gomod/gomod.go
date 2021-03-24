ddc6a611d5ea578bbcc9084ab8e2239c91d1a09b
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-03-24 19:30:14 -0300

    fix: proxy main pkg
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- internal/pipe/gomod/gomod.go
+++ internal/pipe/gomod/gomod.go
@@ -11,6 +11,7 @@ import (
 	"github.com/apex/log"
 	"github.com/goreleaser/goreleaser/internal/pipe"
 	"github.com/goreleaser/goreleaser/internal/tmpl"
+	"github.com/goreleaser/goreleaser/pkg/config"
 	"github.com/goreleaser/goreleaser/pkg/context"
 )
 
@@ -47,12 +48,27 @@ func (Pipe) Run(ctx *context.Context) error {
 }
 
 func setupProxy(ctx *context.Context) error {
-	template := tmpl.New(ctx)
+	for i := range ctx.Config.Builds {
+		build := &ctx.Config.Builds[i]
+		if err := proxyBuild(ctx, build); err != nil {
+			return err
+		}
+	}
 
-	log.Infof("proxying %s@%s", ctx.ModulePath, ctx.Git.CurrentTag)
+	return nil
+}
+
+func proxyBuild(ctx *context.Context, build *config.Build) error {
+	mainPackage := path.Join(ctx.ModulePath, build.Main)
+	template := tmpl.New(ctx).WithExtraFields(tmpl.Fields{
+		"Main":    mainPackage,
+		"BuildID": build.ID,
+	})
+
+	log.Infof("proxying %s@%s to build %s", ctx.ModulePath, ctx.Git.CurrentTag, mainPackage)
 
 	mod, err := template.Apply(`
-module {{ .ProjectName }}
+module {{ .BuildID }}
 
 require {{ .ModulePath }} {{ .Tag }}
 `)
@@ -64,37 +80,38 @@ require {{ .ModulePath }} {{ .Tag }}
 // +build main
 package main
 
-import _ "{{ .ModulePath }}"
+import _ "{{ .Main }}"
 `)
 	if err != nil {
 		return fmt.Errorf("failed to proxy module: %w", err)
 	}
 
-	dir := fmt.Sprintf("%s/proxy", ctx.Config.Dist)
+	dir := fmt.Sprintf("%s/proxy/%s", ctx.Config.Dist, build.ID)
 
 	log.Debugf("creating needed files")
 
-	if err := os.Mkdir(dir, 0o755); err != nil {
+	if err := os.MkdirAll(dir, 0o755); err != nil {
 		return fmt.Errorf("failed to proxy module: %w", err)
 	}
 
-	if err := os.WriteFile(dir+"/main.go", []byte(main), 0o650); err != nil {
+	if err := os.WriteFile(dir+"/main.go", []byte(main), 0o666); err != nil {
 		return fmt.Errorf("failed to proxy module: %w", err)
 	}
 
-	if err := os.WriteFile(dir+"/go.mod", []byte(mod), 0o650); err != nil {
+	if err := os.WriteFile(dir+"/go.mod", []byte(mod), 0o666); err != nil {
 		return fmt.Errorf("failed to proxy module: %w", err)
 	}
 
-	sumr, err := os.OpenFile("go.sum", os.O_RDONLY, 0o650)
+	sumr, err := os.OpenFile("go.sum", os.O_RDONLY, 0o666)
 	if err != nil {
 		return fmt.Errorf("failed to proxy module: %w", err)
 	}
 
-	sumw, err := os.OpenFile(dir+"/go.sum", os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0o650)
+	sumw, err := os.Create(dir + "/go.sum")
 	if err != nil {
 		return fmt.Errorf("failed to proxy module: %w", err)
 	}
+	defer sumw.Close()
 
 	if _, err := io.Copy(sumw, sumr); err != nil {
 		return fmt.Errorf("failed to proxy module: %w", err)
@@ -107,11 +124,7 @@ import _ "{{ .ModulePath }}"
 		return fmt.Errorf("failed to proxy module: %w: %s", err, string(out))
 	}
 
-	for i := range ctx.Config.Builds {
-		build := &ctx.Config.Builds[i]
-		build.Main = path.Join(ctx.ModulePath, build.Main)
-		build.Dir = dir
-	}
-
+	build.Main = mainPackage
+	build.Dir = dir
 	return nil
 }
