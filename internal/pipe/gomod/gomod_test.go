525fdbb3e01c2544139789eb2db7a5fddd2dde42
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-03-29 19:51:39 -0300

    fix: improve test
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- internal/pipe/gomod/gomod_test.go
+++ internal/pipe/gomod/gomod_test.go
@@ -51,21 +51,28 @@ func TestGoModProxy(t *testing.T) {
 		dist := filepath.Join(dir, "dist")
 		ctx := context.New(config.Project{
 			Dist: dist,
+			GoMod: config.GoMod{
+				Proxy: true,
+			},
+			Builds: []config.Build{
+				{
+					ID:     "foo",
+					Goos:   []string{runtime.GOOS},
+					Goarch: []string{runtime.GOARCH},
+					Main:   ".",
+				},
+			},
 		})
 		ctx.Git.CurrentTag = "v0.161.1"
 		ctx.ModulePath = "github.com/goreleaser/goreleaser"
-		build := config.Build{
-			ID:     "foo",
-			Goos:   []string{runtime.GOOS},
-			Goarch: []string{runtime.GOARCH},
-			Main:   ".",
-		}
 
-		setupGoSumFromURL(t, "https://raw.githubusercontent.com/goreleaser/goreleaser/v0.161.1/go.sum")
+		setupGoModSumFromURL(t, "https://raw.githubusercontent.com/goreleaser/goreleaser/v0.161.1")
 		require.NoError(t, Pipe{}.Default(ctx))
-		require.NoError(t, proxyBuild(ctx, &build))
+		require.NoError(t, Pipe{}.Run(ctx))
 		requireGoMod(t, "github.com/goreleaser/goreleaser", "v0.161.1")
 		requireMainGo(t, "github.com/goreleaser/goreleaser")
+		require.Equal(t, "github.com/goreleaser/goreleaser", ctx.Config.Builds[0].Main)
+		require.Equal(t, filepath.Join(dist, "proxy", "foo"), ctx.Config.Builds[0].Dir)
 	})
 
 	t.Run("nfpm", func(t *testing.T) {
@@ -73,25 +80,34 @@ func TestGoModProxy(t *testing.T) {
 		dist := filepath.Join(dir, "dist")
 		ctx := context.New(config.Project{
 			Dist: dist,
+			GoMod: config.GoMod{
+				Proxy: true,
+			},
+			Builds: []config.Build{
+				{
+					ID:     "foo",
+					Goos:   []string{runtime.GOOS},
+					Goarch: []string{runtime.GOARCH},
+					Main:   "./cmd/nfpm",
+				},
+			},
 		})
 		ctx.Git.CurrentTag = "v2.3.1"
 		ctx.ModulePath = "github.com/goreleaser/nfpm/v2"
-		build := config.Build{
-			ID:     "foo",
-			Goos:   []string{runtime.GOOS},
-			Goarch: []string{runtime.GOARCH},
-			Main:   "./cmd/nfpm",
-		}
 
-		setupGoSumFromURL(t, "https://raw.githubusercontent.com/goreleaser/nfpm/v2.3.1/go.sum")
+		setupGoModSumFromURL(t, "https://raw.githubusercontent.com/goreleaser/nfpm/v2.3.1")
 		require.NoError(t, Pipe{}.Default(ctx))
-		require.NoError(t, proxyBuild(ctx, &build))
+		require.NoError(t, Pipe{}.Run(ctx))
 		requireGoMod(t, "github.com/goreleaser/nfpm/v2", "v2.3.1")
 		requireMainGo(t, "github.com/goreleaser/nfpm/v2/cmd/nfpm")
+		require.Equal(t, "github.com/goreleaser/nfpm/v2/cmd/nfpm", ctx.Config.Builds[0].Main)
+		require.Equal(t, filepath.Join(dist, "proxy", "foo"), ctx.Config.Builds[0].Dir)
 	})
 }
 
 func requireGoMod(tb testing.TB, module, version string) {
+	tb.Helper()
+
 	mod, err := os.ReadFile("dist/proxy/foo/go.mod")
 	require.NoError(tb, err)
 	require.Equal(tb, fmt.Sprintf(`module foo
@@ -103,6 +119,8 @@ require %s %s
 }
 
 func requireMainGo(tb testing.TB, module string) {
+	tb.Helper()
+
 	main, err := os.ReadFile("dist/proxy/foo/main.go")
 	require.NoError(tb, err)
 	require.Equal(tb, fmt.Sprintf(`
@@ -113,15 +131,21 @@ import _ "%s"
 `, module), string(main))
 }
 
-func setupGoSumFromURL(tb testing.TB, url string) {
+func setupGoModSumFromURL(tb testing.TB, url string) {
 	tb.Helper()
 
-	reps, err := http.Get(url)
+	require.NoError(tb, os.WriteFile("go.mod", getBody(tb, url+"/go.mod"), 0o666))
+	require.NoError(tb, os.WriteFile("go.sum", getBody(tb, url+"/go.sum"), 0o666))
+}
+
+func getBody(tb testing.TB, url string) []byte {
+	tb.Log("URL", url)
+	res, err := http.Get(url)
 	require.NoError(tb, err)
 	tb.Cleanup(func() {
-		require.NoError(tb, reps.Body.Close())
+		require.NoError(tb, res.Body.Close())
 	})
-	sum, err := ioutil.ReadAll(reps.Body)
+	bts, err := ioutil.ReadAll(res.Body)
 	require.NoError(tb, err)
-	require.NoError(tb, os.WriteFile("go.sum", sum, 0o666))
+	return bts
 }
