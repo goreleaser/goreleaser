d08b7679754b9060692206e2eb82ee5e2395b1b3
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2021-05-20 23:24:44 -0300

    fix: revert "fix: skip releasing to brew when skipping publishing" (#2233)
    This reverts commit c85472a20ff6de666d86ab4a941bdc4ee2194d1e.

--- internal/pipe/brew/brew.go
+++ internal/pipe/brew/brew.go
@@ -48,9 +48,6 @@ func (Pipe) String() string {
 
 // Publish brew formula.
 func (Pipe) Publish(ctx *context.Context) error {
-	if ctx.SkipPublish {
-		return pipe.ErrSkipPublishEnabled
-	}
 	// we keep GitHub as default for now, in line with releases
 	if string(ctx.TokenType) == "" {
 		ctx.TokenType = context.TokenTypeGitHub
