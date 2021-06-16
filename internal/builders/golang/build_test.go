10f3021afe80f1449501f48fc3f9d570bc54c468
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2021-06-16 20:41:52 -0300

    fix:  empty build.tags and build.ldflags (#2301)
    * refactor: build go build cmd line
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>
    
    * fix: build.tags
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>
    
    * refactor: improve test code
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>
    
    * refactor: improve test code
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- internal/builders/golang/build_test.go
+++ internal/builders/golang/build_test.go
@@ -736,21 +736,6 @@ func TestProcessFlagsInvalid(t *testing.T) {
 	require.Nil(t, flags)
 }
 
-func TestJoinLdFlags(t *testing.T) {
-	tests := []struct {
-		input  []string
-		output string
-	}{
-		{[]string{"-s -w -X main.version={{.Version}} -X main.commit={{.Commit}} -X main.date={{.Date}} -X main.builtBy=goreleaser"}, "-ldflags=-s -w -X main.version={{.Version}} -X main.commit={{.Commit}} -X main.date={{.Date}} -X main.builtBy=goreleaser"},
-		{[]string{"-s -w", "-X main.version={{.Version}}"}, "-ldflags=-s -w -X main.version={{.Version}}"},
-	}
-
-	for _, test := range tests {
-		joinedLdFlags := joinLdFlags(test.input)
-		require.Equal(t, joinedLdFlags, test.output)
-	}
-}
-
 func TestBuildModTimestamp(t *testing.T) {
 	// round to seconds since this will be a unix timestamp
 	modTime := time.Now().AddDate(-1, 0, 0).Round(1 * time.Second).UTC()
@@ -817,6 +802,69 @@ func TestBuildModTimestamp(t *testing.T) {
 		require.NoError(t, err)
 		require.True(t, modTime.Equal(fi.ModTime()), "inconsistent mod times found when specifying ModTimestamp")
 	}
+}
+
+func TestBuildGoBuildLine(t *testing.T) {
+	requireEqualCmd := func(tb testing.TB, build config.Build, expected []string) {
+		tb.Helper()
+		config := config.Project{
+			Builds: []config.Build{build},
+		}
+		ctx := context.New(config)
+		ctx.Version = "v1.2.3"
+		ctx.Git.Commit = "aaa"
+
+		line, err := buildGoBuildLine(ctx, config.Builds[0], api.Options{Path: "foo"}, &artifact.Artifact{}, []string{})
+		require.NoError(t, err)
+		require.Equal(t, expected, line)
+	}
+
+	t.Run("full", func(t *testing.T) {
+		requireEqualCmd(t, config.Build{
+			Main:     ".",
+			Asmflags: []string{"asmflag1", "asmflag2"},
+			Gcflags:  []string{"gcflag1", "gcflag2"},
+			Flags:    []string{"-flag1", "-flag2"},
+			Tags:     []string{"tag1", "tag2"},
+			Ldflags:  []string{"ldflag1", "ldflag2"},
+			GoBinary: "go",
+		}, []string{
+			"go", "build",
+			"-flag1", "-flag2",
+			"-asmflags=asmflag1", "-asmflags=asmflag2",
+			"-gcflags=gcflag1", "-gcflags=gcflag2",
+			"-tags=tag1,tag2",
+			"-ldflags=ldflag1 ldflag2",
+			"-o", "foo", ".",
+		})
+	})
+
+	t.Run("simple", func(t *testing.T) {
+		requireEqualCmd(t, config.Build{
+			Main:     ".",
+			GoBinary: "go",
+		}, strings.Fields("go build -o foo ."))
+	})
+
+	t.Run("ldflags1", func(t *testing.T) {
+		requireEqualCmd(t, config.Build{
+			Main:     ".",
+			Ldflags:  []string{"-s -w -X main.version={{.Version}} -X main.commit={{.Commit}} -X main.builtBy=goreleaser"},
+			GoBinary: "go",
+		}, []string{
+			"go", "build",
+			"-ldflags=-s -w -X main.version=v1.2.3 -X main.commit=aaa -X main.builtBy=goreleaser",
+			"-o", "foo", ".",
+		})
+	})
+
+	t.Run("ldflags2", func(t *testing.T) {
+		requireEqualCmd(t, config.Build{
+			Main:     ".",
+			Ldflags:  []string{"-s -w", "-X main.version={{.Version}}"},
+			GoBinary: "go",
+		}, []string{"go", "build", "-ldflags=-s -w -X main.version=v1.2.3", "-o", "foo", "."})
+	})
 }
 
 //
