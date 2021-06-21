f7983ea83ea5cb8e06620a5e35d8503a3872c7ce
Carlos A Becker <caarlos0@gmail.com>
2021-06-21 02:52:31 +0000

    fix: fury upload, maybe
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- scripts/fury-upload.sh
+++ scripts/fury-upload.sh
@@ -2,5 +2,6 @@
 set -e
 if [ "${1: -4}" == ".deb" ] || [ "${1: -4}" == ".rpm" ]; then
 	cd dist
-	curl -F package="@$1" "https://$FURY_TOKEN@push.fury.io/goreleaser/"
+	echo "uploading $1"
+	curl -q -s -F package="@$1" "https://$FURY_TOKEN@push.fury.io/goreleaser/" >/dev/null
 fi
