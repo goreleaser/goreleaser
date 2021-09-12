e1c52c454515f4759cd22870e366b44c81b374ba
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-09-11 21:16:04 -0300

    refactor: improve middleware
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- internal/middleware/logging/logging.go
+++ internal/middleware/logging/logging.go
@@ -1,9 +1,10 @@
-package middleware
+package logging
 
 import (
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/cli"
 	"github.com/fatih/color"
+	"github.com/goreleaser/goreleaser/internal/middleware"
 	"github.com/goreleaser/goreleaser/pkg/context"
 )
 
@@ -16,13 +17,13 @@ const DefaultInitialPadding Padding = 3
 // ExtraPadding is the double of the DefaultInitialPadding.
 const ExtraPadding = DefaultInitialPadding * 2
 
-// Logging pretty prints the given action and its title.
+// Log pretty prints the given action and its title.
 // You can have different padding levels by providing different initial
 // paddings. The middleware will print the title in the given padding and the
 // action logs in padding+default padding.
 // The default padding in the log library is 3.
 // The middleware always resets to the default padding.
-func Logging(title string, next Action, padding Padding) Action {
+func Log(title string, next middleware.Action, padding Padding) middleware.Action {
 	return func(ctx *context.Context) error {
 		defer func() {
 			cli.Default.Padding = int(DefaultInitialPadding)
