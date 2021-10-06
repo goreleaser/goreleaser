c456654778c094462263c37d06e6f08ee403a3d8
Carlos A Becker <caarlos0@gmail.com>
2021-10-06 01:16:05 -0300

    fix: ignore 409 when pushing to fury.io
    closes #2560
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- scripts/fury-upload.sh
+++ scripts/fury-upload.sh
@@ -3,5 +3,10 @@ set -e
 if [ "${1: -4}" == ".deb" ] || [ "${1: -4}" == ".rpm" ]; then
 	cd dist
 	echo "uploading $1"
-	curl -f -q -s -F package="@$1" "https://$FURY_TOKEN@push.fury.io/goreleaser/" >/dev/null
+	status="$(curl -s -q -o /dev/null -w "%{http_code}" -F package="@$1" "https://$FURY_TOKEN@push.fury.io/goreleaser/")"
+	echo "got: $status"
+	if [ "$status" == "200" ] || [ "$status" == "409" ]; then
+		exit 0
+	fi
+	exit 1
 fi
