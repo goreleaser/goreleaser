baa41547e88ee1c38525fa396d4d28cd6d27f369
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2021-10-06 15:07:00 +0200

    feat: add telegram announcer

---
+++ internal/pipe/telegram/telegram.go
@@ -0,0 +1,57 @@
+package telegram
+
+import (
+	"fmt"
+
+	"github.com/apex/log"
+	"github.com/caarlos0/env/v6"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/goreleaser/goreleaser/internal/tmpl"
+	"github.com/goreleaser/goreleaser/pkg/context"
+)
+
+const defaultMessageTemplate = `{{ .ProjectName }} {{ .Tag }} is out! Check it out at {{ .GitURL }}/releases/tag/{{ .Tag }}`
+
+type Pipe struct{}
+
+func (Pipe) String() string                 { return "telegram" }
+func (Pipe) Skip(ctx *context.Context) bool { return !ctx.Config.Announce.Telegram.Enabled }
+
+type Config struct {
+	ConsumerToken   string `env:"TELEGRAM_TOKEN,notEmpty"`
+	WebHookUrl      string `env:"WEB_HOOK_URL,notEmpty"`
+	CertificateFile string `env:"CERT_FILE,notEmpty"`
+}
+
+func (Pipe) Default(ctx *context.Context) error {
+	if ctx.Config.Announce.Telegram.MessageTemplate == "" {
+		ctx.Config.Announce.Telegram.MessageTemplate = defaultMessageTemplate
+	}
+	return nil
+}
+
+func (Pipe) Announce(ctx *context.Context) error {
+	msg, err := tmpl.New(ctx).Apply(ctx.Config.Announce.Telegram.MessageTemplate)
+	if err != nil {
+		return fmt.Errorf("announce: failed to announce to telegram: %w", err)
+	}
+
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		return fmt.Errorf("announce: failed to announce to telegram: %w", err)
+	}
+
+	log.Infof("posting: '%s'", msg)
+	bot, err := tgbotapi.NewBotAPI(cfg.ConsumerToken)
+
+	if err != nil {
+		return fmt.Errorf("announce: failed to announce to telegram: %w", err)
+	}
+
+	wc := tgbotapi.NewWebhookWithCert(cfg.WebHookUrl, cfg.CertificateFile)
+	_, err = bot.SetWebhook(wc)
+	if err != nil {
+		return fmt.Errorf("announce: failed to announce to telegram: %w", err)
+	}
+	return nil
+}
