50754c18c66eb79c9a3238a3dea7ab1bedfdf422
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2021-10-20 08:34:31 +0200

    chore: remove output

--- internal/pipe/docker/api_docker.go
+++ internal/pipe/docker/api_docker.go
@@ -51,7 +51,6 @@ func (i dockerImager) Push(ctx context.Context, image string, flags []string) er
 }
 
 func (i dockerImager) Build(ctx context.Context, envs map[string]string, root string, images, flags []string) error {
-	fmt.Println(envs["KO_DOCKER_REPO"])
 	if err := runCommand(ctx, root, "docker", i.buildCommand(images, flags)...); err != nil {
 		return fmt.Errorf("failed to build %s: %w", images[0], err)
 	}
