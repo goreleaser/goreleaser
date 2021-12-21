b21d56d5d983770441fcd196f1db5405ab912115
Carlos A Becker <caarlos0@gmail.com>
2021-12-20 21:34:34 -0300

    test: fix version test
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- main_test.go
+++ main_test.go
@@ -1,40 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestVersion(t *testing.T) {
+	os := fmt.Sprintf("\ngoos: %s\ngoarch: %s", runtime.GOOS, runtime.GOARCH)
 	for name, tt := range map[string]struct {
 		version, commit, date, builtBy string
 		out                            string
 	}{
 		"all empty": {
-			out: website,
+			out: os + website,
 		},
 		"complete": {
 			version: "1.2.3",
 			date:    "12/12/12",
 			commit:  "aaaa",
 			builtBy: "me",
-			out:     "1.2.3\ncommit: aaaa\nbuilt at: 12/12/12\nbuilt by: me" + website,
+			out:     "1.2.3\ncommit: aaaa\nbuilt at: 12/12/12\nbuilt by: me" + os + website,
 		},
 		"only version": {
 			version: "1.2.3",
-			out:     "1.2.3" + website,
+			out:     "1.2.3" + os + website,
 		},
 		"version and date": {
 			version: "1.2.3",
 			date:    "12/12/12",
-			out:     "1.2.3\nbuilt at: 12/12/12" + website,
+			out:     "1.2.3\nbuilt at: 12/12/12" + os + website,
 		},
 		"version, date, built by": {
 			version: "1.2.3",
 			date:    "12/12/12",
 			builtBy: "me",
-			out:     "1.2.3\nbuilt at: 12/12/12\nbuilt by: me" + website,
+			out:     "1.2.3\nbuilt at: 12/12/12\nbuilt by: me" + os + website,
 		},
 	} {
 		tt := tt
