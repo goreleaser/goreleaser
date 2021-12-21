440e9a63b93b3d982b6f81cc2047dedb3ce4238b
Carlos A Becker <caarlos0@gmail.com>
2021-12-21 00:07:17 -0300

    test: fixed snapcraft test
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- internal/gio/hash_test.go
+++ internal/gio/hash_test.go
@@ -18,5 +18,44 @@ func TestEqualFilesError(t *testing.T) {
 		equal, err := EqualFiles(test.a, test.b)
 		require.Error(t, err)
 		require.False(t, equal)
+
+		equalContents, err := EqualFileContents(test.a, test.b)
+		require.Error(t, err)
+		require.False(t, equalContents)
+	}
+}
+
+func TestEqualFiles(t *testing.T) {
+	tests := []struct {
+		a string
+		b string
+	}{
+		{"./testdata/somefile.txt", "./testdata/somefile_copy.txt"},
+	}
+	for _, test := range tests {
+		equal, err := EqualFiles(test.a, test.b)
+		require.NoError(t, err)
+		require.True(t, equal)
+
+		equalContents, err := EqualFileContents(test.a, test.b)
+		require.NoError(t, err)
+		require.True(t, equalContents)
+	}
+}
+func TestEqualFileCointents(t *testing.T) {
+	tests := []struct {
+		a string
+		b string
+	}{
+		{"./testdata/somefile.txt", "./testdata/somefile_copy_perm.txt"},
+	}
+	for _, test := range tests {
+		equal, err := EqualFiles(test.a, test.b)
+		require.NoError(t, err)
+		require.False(t, equal)
+
+		equalContents, err := EqualFileContents(test.a, test.b)
+		require.NoError(t, err)
+		require.True(t, equalContents)
 	}
 }
