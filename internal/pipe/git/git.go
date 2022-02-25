2c91a48d91dd7e0ee6d131af1da18e9cc29808b0
Carlos A Becker <caarlos0@gmail.com>
2022-02-24 22:34:04 -0300

    refactor: tag contents
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- internal/pipe/git/git.go
+++ internal/pipe/git/git.go
@@ -116,17 +116,17 @@ func getGitInfo() (context.GitInfo, error) {
 		}, ErrNoTag
 	}
 
-	subject, err := getTagSubject(tag)
+	subject, err := getTagWithFormat(tag, "contents:subject")
 	if err != nil {
 		return context.GitInfo{}, fmt.Errorf("couldn't get tag subject: %w", err)
 	}
 
-	contents, err := getTagContents(tag, "")
+	contents, err := getTagWithFormat(tag, "contents")
 	if err != nil {
 		return context.GitInfo{}, fmt.Errorf("couldn't get tag contents: %w", err)
 	}
 
-	body, err := getTagContents(tag, ":body")
+	body, err := getTagWithFormat(tag, "contents:body")
 	if err != nil {
 		return context.GitInfo{}, fmt.Errorf("couldn't get tag content body: %w", err)
 	}
@@ -217,12 +217,8 @@ func getSummary() (string, error) {
 	return git.Clean(git.Run("describe", "--always", "--dirty", "--tags"))
 }
 
-func getTagSubject(tag string) (string, error) {
-	return git.Clean(git.Run("tag", "-l", "--format='%(contents:subject)'", tag))
-}
-
-func getTagContents(tag, format string) (string, error) {
-	out, err := git.Run("tag", "-l", "--format='%(contents"+format+")'", tag)
+func getTagWithFormat(tag, format string) (string, error) {
+	out, err := git.Run("tag", "-l", "--format='%("+format+")'", tag)
 	return strings.TrimSpace(strings.TrimSuffix(strings.ReplaceAll(out, "'", ""), "\n\n")), err
 }
 
