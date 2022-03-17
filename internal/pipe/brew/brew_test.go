4edd6b20ed44b83c2f6f187bc140c2821ca307a0
Carlos A Becker <caarlos0@gmail.com>
2022-03-16 21:29:43 -0300

    fix: brew.post_install indent
    properly indent brews.post_install block, and also fix indent on newly
    added brews.service block.
    
    closes #2980
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- internal/pipe/brew/brew_test.go
+++ internal/pipe/brew/brew_test.go
@@ -103,7 +103,7 @@ func TestFullFormulae(t *testing.T) {
 	data.Dependencies = []config.HomebrewDependency{{Name: "gtk+"}}
 	data.Conflicts = []string{"svn"}
 	data.Plist = "it works"
-	data.PostInstall = `system "touch", "/tmp/foo"`
+	data.PostInstall = []string{`system "touch", "/tmp/foo"`, `system "echo", "done"`}
 	data.CustomBlock = []string{"devel do", `  url "https://github.com/caarlos0/test/releases/download/v0.1.3/test_Darwin_x86_64.tar.gz"`, `  sha256 "1633f61598ab0791e213135923624eb342196b3494909c91899bcd0560f84c68"`, "end"}
 	data.Tests = []string{`system "#{bin}/{{.ProjectName}} -version"`}
 	formulae, err := doBuildFormula(context.New(config.Project{
@@ -275,6 +275,7 @@ func TestFullPipe(t *testing.T) {
 							Dependencies: []config.HomebrewDependency{{Name: "zsh", Type: "optional"}, {Name: "bash"}},
 							Conflicts:    []string{"gtk+", "qt"},
 							Service:      "run foo/bar\nkeep_alive true",
+							PostInstall:  "system \"echo\"\nsystem \"touch\" \"/tmp/hi\"",
 							Install:      `bin.install "{{ .ProjectName }}"`,
 						},
 					},
