48671499dfd35e3256bc8c57b5b4bf57563a3c57
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-06-11 00:40:52 -0300

    test: fixes
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- internal/semerrgroup/sem_test.go
+++ internal/semerrgroup/sem_test.go
@@ -11,20 +11,24 @@ import (
 )
 
 func TestSemaphore(t *testing.T) {
-	g := New(4)
-	var lock sync.Mutex
-	var counter int
-	for i := 0; i < 10; i++ {
-		g.Go(func() error {
-			time.Sleep(10 * time.Millisecond)
-			lock.Lock()
-			counter++
-			lock.Unlock()
-			return nil
+	for _, i := range []int{1, 4} {
+		t.Run(fmt.Sprintf("limit-%d", i), func(t *testing.T) {
+			g := New(i)
+			var lock sync.Mutex
+			var counter int
+			for i := 0; i < 10; i++ {
+				g.Go(func() error {
+					time.Sleep(10 * time.Millisecond)
+					lock.Lock()
+					counter++
+					lock.Unlock()
+					return nil
+				})
+			}
+			require.NoError(t, g.Wait())
+			require.Equal(t, counter, 10)
 		})
 	}
-	require.NoError(t, g.Wait())
-	require.Equal(t, counter, 10)
 }
 
 func TestSemaphoreOrder(t *testing.T) {
@@ -42,39 +46,47 @@ func TestSemaphoreOrder(t *testing.T) {
 	require.Equal(t, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, output)
 }
 
-func TestSemaphoreOrderError(t *testing.T) {
-	g := New(1)
-	output := []int{}
-	for i := 0; i < 10; i++ {
-		i := i
-		g.Go(func() error {
-			output = append(output, i)
-			return fmt.Errorf("fake err")
+func TestSemaphoreError(t *testing.T) {
+	for _, i := range []int{1, 4} {
+		t.Run(fmt.Sprintf("limit-%d", i), func(t *testing.T) {
+			g := New(i)
+			output := []int{}
+			for i := 0; i < 10; i++ {
+				i := i
+				g.Go(func() error {
+					output = append(output, i)
+					return fmt.Errorf("fake err")
+				})
+			}
+			require.EqualError(t, g.Wait(), "fake err")
+			require.Len(t, output, 10)
 		})
 	}
-	require.EqualError(t, g.Wait(), "fake err")
-	require.Equal(t, []int{0}, output)
 }
 
 func TestSemaphoreSkipAware(t *testing.T) {
-	g := NewSkipAware(New(1))
-	var lock sync.Mutex
-	var counter int
-	for i := 0; i < 10; i++ {
-		g.Go(func() error {
-			time.Sleep(10 * time.Millisecond)
-			lock.Lock()
-			counter++
-			lock.Unlock()
-			return pipe.Skip("fake skip")
+	for _, i := range []int{1, 4} {
+		t.Run(fmt.Sprintf("limit-%d", i), func(t *testing.T) {
+			g := NewSkipAware(New(i))
+			var lock sync.Mutex
+			var counter int
+			for i := 0; i < 10; i++ {
+				g.Go(func() error {
+					time.Sleep(10 * time.Millisecond)
+					lock.Lock()
+					counter++
+					lock.Unlock()
+					return pipe.Skip("fake skip")
+				})
+			}
+			require.EqualError(t, g.Wait(), "fake skip")
+			require.Equal(t, counter, 10)
 		})
 	}
-	require.EqualError(t, g.Wait(), "fake skip")
-	require.Equal(t, counter, 10)
 }
 
 func TestSemaphoreSkipAndRealError(t *testing.T) {
-	g := NewSkipAware(New(10))
+	g := NewSkipAware(New(20))
 	for i := 0; i < 100; i++ {
 		g.Go(func() error {
 			time.Sleep(10 * time.Millisecond)
