6a67ac420e8e6b4fc836b96f3141d62102a44f6e
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-06-24 00:13:08 -0300

    chore(ci): improve tparse output
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -o pipefail && TEST_OPTIONS="-json" task test | tee output.json | tparse -follow -all
+set -o pipefail && TEST_OPTIONS="-json" task test | tee output.json | tparse -follow
 success=$?
 
 set -e
