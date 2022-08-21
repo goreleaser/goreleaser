a1e681c9ba74054f07ea16a6279d2651b4b0a2f7
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2022-08-21 10:55:41 -0300

    feat(deps): update github to v46 (#3326)
    update to latest
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- internal/client/github.go
+++ internal/client/github.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 
 	"github.com/caarlos0/log"
-	"github.com/google/go-github/v45/github"
+	"github.com/google/go-github/v46/github"
 	"github.com/goreleaser/goreleaser/internal/artifact"
 	"github.com/goreleaser/goreleaser/internal/tmpl"
 	"github.com/goreleaser/goreleaser/pkg/config"
