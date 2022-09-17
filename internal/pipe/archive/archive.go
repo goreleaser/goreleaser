91c6f75b8fb5d8a1ef0284638b84f387dc0ee3f9
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-09-17 00:14:59 -0300

    chore: ign linter warning
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- internal/pipe/archive/archive.go
+++ internal/pipe/archive/archive.go
@@ -32,7 +32,7 @@ const (
 // ErrArchiveDifferentBinaryCount happens when an archive uses several builds which have different goos/goarch/etc sets,
 // causing the archives for some platforms to have more binaries than others.
 // GoReleaser breaks in these cases as it will only cause confusion to other users.
-var ErrArchiveDifferentBinaryCount = errors.New("archive has different count of binaries for each platform, which may cause your users confusion.\nLearn more at https://goreleaser.com/errors/multiple-binaries-archive\n")
+var ErrArchiveDifferentBinaryCount = errors.New("archive has different count of binaries for each platform, which may cause your users confusion.\nLearn more at https://goreleaser.com/errors/multiple-binaries-archive\n") // nolint:revive
 
 // nolint: gochecknoglobals
 var lock sync.Mutex
