9ae87d0d3e926d70eb8238f3cc7b9d72e5605671
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-11-29 08:05:25 -0300

    fix: warn if list has multiple files with same name
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- internal/artifact/artifact.go
+++ internal/artifact/artifact.go
@@ -289,6 +289,17 @@ func New() Artifacts {
 func (artifacts Artifacts) List() []*Artifact {
 	artifacts.lock.Lock()
 	defer artifacts.lock.Unlock()
+	names := map[string]bool{}
+	for _, item := range artifacts.items {
+		if item.Name == "" {
+			continue
+		}
+		if names[item.Name] {
+			log.WithField("name", item.Name).
+				Warn("multiple artifacts with the same name: this may cause errors")
+		}
+		names[item.Name] = true
+	}
 	return artifacts.items
 }
 
