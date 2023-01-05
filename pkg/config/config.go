13fda95e0ebf812c70a405e78dfa57e6c64c1b12
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-01-04 22:31:46 -0300

    docs: update schema for changelog.sort
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -826,7 +826,7 @@ type Filters struct {
 // Changelog Config.
 type Changelog struct {
 	Filters Filters          `yaml:"filters,omitempty" json:"filters,omitempty"`
-	Sort    string           `yaml:"sort,omitempty" json:"sort,omitempty"`
+	Sort    string           `yaml:"sort,omitempty" json:"sort,omitempty" jsonschema:"enum=asc,enum=desc,enum=,default="`
 	Skip    bool             `yaml:"skip,omitempty" json:"skip,omitempty"` // TODO(caarlos0): rename to Disable to match other pipes
 	Use     string           `yaml:"use,omitempty" json:"use,omitempty" jsonschema:"enum=git,enum=github,enum=github-native,enum=gitlab,default=git"`
 	Groups  []ChangeLogGroup `yaml:"groups,omitempty" json:"groups,omitempty"`
