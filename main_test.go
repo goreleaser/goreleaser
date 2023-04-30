fcd1df450fa36401f9e1f4047613904a31c82112
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-04-30 16:53:13 +0000

    test: added tests
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -3,9 +3,41 @@ package main
 import (
 	"testing"
 
+	"github.com/goreleaser/goreleaser/internal/golden"
 	"github.com/stretchr/testify/require"
 )
 
 func TestVersion(t *testing.T) {
-	require.NotEmpty(t, buildVersion("test", "test", "test", "test").String())
+	for name, tt := range map[string]struct {
+		version, commit, date, builtBy string
+	}{
+		"all empty": {},
+		"complete": {
+			version: "1.2.3",
+			date:    "12/12/12",
+			commit:  "aaaa",
+			builtBy: "me",
+		},
+		"only version": {
+			version: "1.2.3",
+		},
+		"version and date": {
+			version: "1.2.3",
+			date:    "12/12/12",
+		},
+		"version, date, built by": {
+			version: "1.2.3",
+			date:    "12/12/12",
+			builtBy: "me",
+		},
+	} {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			v := buildVersion(tt.version, tt.commit, tt.date, tt.builtBy)
+			out, err := v.JSONString()
+			require.NoError(t, err)
+
+			golden.RequireEqualJSON(t, []byte(out))
+		})
+	}
 }
