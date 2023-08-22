f4df94879bc2423d3ca065bac86d5ab8c0eb78d4
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-08-22 01:31:45 +0000

    test: improve testlib.RequireTemplateError check

--- internal/testlib/tmpl.go
+++ internal/testlib/tmpl.go
@@ -2,6 +2,7 @@ package testlib
 
 import (
 	"testing"
+	"text/template"
 
 	"github.com/stretchr/testify/require"
 )
@@ -11,6 +12,5 @@ func RequireTemplateError(tb testing.TB, err error) {
 	tb.Helper()
 
 	require.Error(tb, err)
-	require.Contains(tb, err.Error(), "template:")
-	require.Regexp(tb, "bad character|map has no entry|unexpected \"}\" in operand", err.Error())
+	require.ErrorAs(tb, err, &template.ExecError{})
 }
