4aed6457d7eb19f2b7d321cac2f42db8fe530765
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-09-14 13:02:42 +0000

    ci: test on windows
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/pipe/env/env_test.go
+++ internal/pipe/env/env_test.go
@@ -300,7 +300,7 @@ func TestLoadEnv(t *testing.T) {
 		err = os.Chmod(f.Name(), 0o377)
 		require.NoError(t, err)
 		v, err := loadEnv(env, f.Name())
-		require.EqualError(t, err, fmt.Sprintf("open %s: permission denied", f.Name()))
+		require.ErrorIs(t, err, syscall.EACCES)
 		require.Equal(t, "", v)
 	})
 }
