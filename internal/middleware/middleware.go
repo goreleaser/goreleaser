4aec6b6ec9855ee02c3bb58ef23afe149bd4c536
standstaff <zhengxingru@yeah.net>
2024-03-15 17:43:09 +0800

    chore: remove repetitive words
    Signed-off-by: standstaff <zhengxingru@yeah.net>

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -4,6 +4,6 @@ package middleware
 import "github.com/goreleaser/goreleaser/pkg/context"
 
 // Action is a function that takes a context and returns an error.
-// It is is used on Pipers, Defaulters and Publishers, although they are not
+// It is used on Pipers, Defaulters and Publishers, although they are not
 // aware of this generalization.
 type Action func(ctx *context.Context) error
