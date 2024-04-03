85cb59c1a0236008a96368b677e38b8fd9f8573b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-04-03 14:16:25 -0300

    build: simplify (#4748)
    simplify test scripts a bit
    
    - remove tparse
    - don't verbose test
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o pipefail && TEST_OPTIONS="-json" task test | tee output.json | tparse -follow
-success=$?
-
-set -e
-NO_COLOR=1 tparse -format markdown -slow 10 -file output.json >$GITHUB_STEP_SUMMARY
-
-exit $success
