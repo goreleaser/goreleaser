bee8665d9e6d44277c382e14792584ad5c44f083
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-04-28 20:36:28 -0300

    fix(brew): cpu conditionals (#4814)
    closes #4813
    
    ---------
    
    Co-authored-by: William Walker <w_walker@icloud.com>

--- internal/pipe/brew/templates/macos_packages.rb
+++ internal/pipe/brew/templates/macos_packages.rb
@@ -42,10 +42,10 @@
   end
   {{- else }}
   {{- if eq $element.Arch "amd64" }}
-  if Hardware::CPU.intel?
+  on_intel do
   {{- end }}
   {{- if eq $element.Arch "arm64" }}
-  if Hardware::CPU.arm?
+  on_arm do
   {{- end}}
     url "{{ $element.DownloadURL }}"
     {{- if .DownloadStrategy }}, using: {{ .DownloadStrategy }}{{- end }}
