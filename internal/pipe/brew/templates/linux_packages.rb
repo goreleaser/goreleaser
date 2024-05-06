14ab45930090e4da037591901a12471de9f08db7
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-05-05 23:04:22 -0300

    fix(brew): linux brew with extra end
    refs #4822
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/pipe/brew/templates/linux_packages.rb
+++ internal/pipe/brew/templates/linux_packages.rb
@@ -2,29 +2,29 @@
 {{- range $element := .LinuxPackages }}
   {{- if eq $element.Arch "amd64" }}
   on_intel do
+    if Hardware::CPU.is_64_bit?
   {{- end }}
-  {{- if eq $element.Arch "arm" }}
+  {{- if or (eq $element.Arch "arm") (eq $element.Arch "arm64") }}
   on_arm do
+  {{- if eq $element.Arch "arm64" }}
+    if Hardware::CPU.is_64_bit?
+  {{- else }}
     if !Hardware::CPU.is_64_bit?
-    {{- end }}
-    {{- if eq $element.Arch "arm64" }}
-    on_arm do
-      if Hardware::CPU.is_64_bit?
+  {{- end }}
+  {{- end }}
+      url "{{ $element.DownloadURL }}"
+      {{- if .DownloadStrategy }}, using: {{ .DownloadStrategy }}{{- end }}
+      {{- if .Headers }},
+        headers: [{{ printf "\n" }}
+          {{- join .Headers | indent 10 }}
+        ]
       {{- end }}
-        url "{{ $element.DownloadURL }}"
-        {{- if .DownloadStrategy }}, using: {{ .DownloadStrategy }}{{- end }}
-        {{- if .Headers }},
-          headers: [{{ printf "\n" }}
-            {{- join .Headers | indent 10 }}
-          ]
-        {{- end }}
-        sha256 "{{ $element.SHA256 }}"
+      sha256 "{{ $element.SHA256 }}"
 
-        def install
-          {{- range $index, $element := .Install }}
-          {{ . -}}
-          {{- end }}
-        end
+      def install
+        {{- range $index, $element := .Install }}
+        {{ . -}}
+        {{- end }}
       end
     end
   end
