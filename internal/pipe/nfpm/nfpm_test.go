feab3c4dd15c698dc407b93b23c69855fae62703
rsteube <rsteube@users.noreply.github.com>
2024-05-31 17:56:05 +0200

    termux: support arm

--- internal/pipe/nfpm/nfpm_test.go
+++ internal/pipe/nfpm/nfpm_test.go
@@ -389,7 +389,7 @@ func TestRunPipe(t *testing.T) {
 	}
 	require.NoError(t, Pipe{}.Run(ctx))
 	packages := ctx.Artifacts.Filter(artifact.ByType(artifact.LinuxPackage)).List()
-	require.Len(t, packages, 44)
+	require.Len(t, packages, 46)
 	for _, pkg := range packages {
 		format := pkg.Format()
 		require.NotEmpty(t, format)
