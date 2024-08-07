9332754c788c1d92780948b91397d2ba1bdc177a
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-08-07 13:50:59 -0300

    feat: when --clean, remove dist before checking git state
    closes #5023
    closes #5046

--- internal/pipe/dist/dist_test.go
+++ internal/pipe/dist/dist_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/goreleaser/goreleaser/v2/internal/testctx"
 	"github.com/goreleaser/goreleaser/v2/pkg/config"
+	"github.com/goreleaser/goreleaser/v2/pkg/context"
 	"github.com/stretchr/testify/require"
 )
 
@@ -25,7 +26,7 @@ func TestPopulatedDistExists(t *testing.T) {
 	require.NoError(t, f.Close())
 	ctx := testctx.NewWithCfg(config.Project{Dist: dist})
 	require.Error(t, Pipe{}.Run(ctx))
-	ctx.Clean = true
+	require.NoError(t, CleanPipe{}.Run(ctx))
 	require.NoError(t, Pipe{}.Run(ctx))
 	_, err = os.Stat(dist)
 	require.False(t, os.IsExist(err))
@@ -41,6 +42,24 @@ func TestEmptyDistExists(t *testing.T) {
 	require.False(t, os.IsNotExist(err))
 }
 
-func TestDescription(t *testing.T) {
+func TestString(t *testing.T) {
 	require.NotEmpty(t, Pipe{}.String())
+	require.NotEmpty(t, CleanPipe{}.String())
+}
+
+func TestCleanSkip(t *testing.T) {
+	t.Run("skip", func(t *testing.T) {
+		require.True(t, CleanPipe{}.Skip(testctx.New()))
+	})
+	t.Run("don't skip", func(t *testing.T) {
+		require.False(t, CleanPipe{}.Skip(testctx.New(func(ctx *context.Context) {
+			ctx.Clean = true
+		})))
+	})
+}
+
+func TestCleanSetDist(t *testing.T) {
+	ctx := testctx.New()
+	require.NoError(t, CleanPipe{}.Run(ctx))
+	require.Equal(t, "dist", ctx.Config.Dist)
 }
