d3b70f6351440f729d65ec2305471b091d3b2bb8
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-08-30 09:37:51 -0300

    test: check if pre-requisites are installed
    refs #5104

--- internal/testlib/path.go
+++ internal/testlib/path.go
@@ -6,14 +6,19 @@ import (
 	"testing"
 )
 
-// CheckPath skips the test if the binary is not in the PATH.
+// CheckPath skips the test if the binary is not in the PATH, or if CI is true.
 func CheckPath(tb testing.TB, cmd string) {
 	tb.Helper()
-	if os.Getenv("CI") == "true" {
-		// never skip on CI
-		return
-	}
-	if _, err := exec.LookPath(cmd); err != nil {
+	if !InPath(cmd) {
 		tb.Skipf("%s not in PATH", cmd)
 	}
+}
+
+// InPath returns true if the given cmd is in the PATH, or if CI is true.
+func InPath(cmd string) bool {
+	if os.Getenv("CI") == "true" {
+		return true
+	}
+	_, err := exec.LookPath(cmd)
+	return err == nil
 }
