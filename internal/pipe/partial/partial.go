21df3b8f6f3f12c91620cec976a619c2922612fc
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-09-03 21:02:39 -0300

    fix: build --single-target filters
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/pipe/partial/partial.go
+++ internal/pipe/partial/partial.go
@@ -3,6 +3,7 @@ package partial
 import (
 	"os"
 	"runtime"
+	"strings"
 
 	"github.com/charmbracelet/x/exp/ordered"
 	"github.com/goreleaser/goreleaser/v2/pkg/context"
@@ -21,5 +22,22 @@ func (Pipe) Run(ctx *context.Context) error {
 func getFilter() string {
 	goos := ordered.First(os.Getenv("GGOOS"), os.Getenv("GOOS"), runtime.GOOS)
 	goarch := ordered.First(os.Getenv("GGOARCH"), os.Getenv("GOARCH"), runtime.GOARCH)
-	return goos + "_" + goarch
+	target := goos + "_" + goarch
+
+	if strings.HasSuffix(target, "_amd64") {
+		goamd64 := ordered.First(os.Getenv("GGOAMD64"), os.Getenv("GOAMD64"), "v1")
+		target = target + "_" + goamd64
+	}
+	if strings.HasSuffix(target, "_arm") {
+		goarm := ordered.First(os.Getenv("GGOARM"), os.Getenv("GOARM"), "6")
+		target = target + "_" + goarm
+	}
+	if strings.HasSuffix(target, "_mips") ||
+		strings.HasSuffix(target, "_mips64") ||
+		strings.HasSuffix(target, "_mipsle") ||
+		strings.HasSuffix(target, "_mips64le") {
+		gomips := ordered.First(os.Getenv("GGOMIPS"), os.Getenv("GOMIPS"), "hardfloat")
+		target = target + "_" + gomips
+	}
+	return target
 }
--- internal/pipe/partial/partial_test.go
+++ internal/pipe/partial/partial_test.go
@@ -46,12 +46,68 @@ func TestRun(t *testing.T) {
 		require.NoError(t, pipe.Run(ctx))
 		require.Equal(t, "windows_arm64", ctx.PartialTarget)
 	})
+	t.Run("custom GGOARM", func(t *testing.T) {
+		ctx := testctx.NewWithCfg(config.Project{
+			Dist: "dist",
+		}, testctx.Partial)
+		t.Setenv("GGOOS", "linux")
+		t.Setenv("GGOARCH", "arm")
+		t.Run("default", func(t *testing.T) {
+			require.NoError(t, pipe.Run(ctx))
+			require.Equal(t, "linux_arm_6", ctx.PartialTarget)
+		})
+		t.Run("default", func(t *testing.T) {
+			t.Setenv("GGOARM", "7")
+			require.NoError(t, pipe.Run(ctx))
+			require.Equal(t, "linux_arm_7", ctx.PartialTarget)
+		})
+	})
+	t.Run("custom GGOAMD64", func(t *testing.T) {
+		ctx := testctx.NewWithCfg(config.Project{
+			Dist: "dist",
+		}, testctx.Partial)
+		t.Setenv("GGOOS", "linux")
+		t.Setenv("GGOARCH", "amd64")
+		t.Run("default", func(t *testing.T) {
+			require.NoError(t, pipe.Run(ctx))
+			require.Equal(t, "linux_amd64_v1", ctx.PartialTarget)
+		})
+		t.Run("default", func(t *testing.T) {
+			t.Setenv("GGOAMD64", "v4")
+			require.NoError(t, pipe.Run(ctx))
+			require.Equal(t, "linux_amd64_v4", ctx.PartialTarget)
+		})
+	})
+	t.Run("custom GGOMIPS", func(t *testing.T) {
+		ctx := testctx.NewWithCfg(config.Project{
+			Dist: "dist",
+		}, testctx.Partial)
+		t.Setenv("GGOOS", "linux")
+		for _, mips := range []string{"mips", "mips64", "mipsle", "mips64le"} {
+			t.Run(mips, func(t *testing.T) {
+				t.Setenv("GGOARCH", mips)
+				t.Run("default", func(t *testing.T) {
+					require.NoError(t, pipe.Run(ctx))
+					require.Equal(t, "linux_"+mips+"_hardfloat", ctx.PartialTarget)
+				})
+				t.Run("default", func(t *testing.T) {
+					t.Setenv("GGOMIPS", "softfloat")
+					require.NoError(t, pipe.Run(ctx))
+					require.Equal(t, "linux_"+mips+"_softfloat", ctx.PartialTarget)
+				})
+			})
+		}
+	})
 	t.Run("using runtime", func(t *testing.T) {
 		ctx := testctx.NewWithCfg(config.Project{
 			Dist: "dist",
 		}, testctx.Partial)
 		require.NoError(t, pipe.Run(ctx))
 		target := fmt.Sprintf("%s_%s", runtime.GOOS, runtime.GOARCH)
+		switch runtime.GOARCH {
+		case "amd64":
+			target += "_v1"
+		}
 		require.Equal(t, target, ctx.PartialTarget)
 	})
 }
