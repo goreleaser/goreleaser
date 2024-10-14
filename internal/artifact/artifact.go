d583861e0606f2bb9e97c0d0e28f9b82df6a187a
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-10-14 09:40:10 -0300

    feat(build): add GO386, GOMIPS64, GORISCV64, and GOPPC64 support (#5186)
    continuing the work of #5153
    
    closes #5153
    
    ---------
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
    Co-authored-by: 世界 <i@sekai.icu>

--- internal/artifact/artifact.go
+++ internal/artifact/artifact.go
@@ -181,16 +181,20 @@ func (e Extras) MarshalJSON() ([]byte, error) {
 
 // Artifact represents an artifact and its relevant info.
 type Artifact struct {
-	Name    string `json:"name,omitempty"`
-	Path    string `json:"path,omitempty"`
-	Goos    string `json:"goos,omitempty"`
-	Goarch  string `json:"goarch,omitempty"`
-	Goarm   string `json:"goarm,omitempty"`
-	Gomips  string `json:"gomips,omitempty"`
-	Goamd64 string `json:"goamd64,omitempty"`
-	Type    Type   `json:"internal_type,omitempty"`
-	TypeS   string `json:"type,omitempty"`
-	Extra   Extras `json:"extra,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Path      string `json:"path,omitempty"`
+	Goos      string `json:"goos,omitempty"`
+	Goarch    string `json:"goarch,omitempty"`
+	Goamd64   string `json:"goamd64,omitempty"`
+	Go386     string `json:"go386,omitempty"`
+	Goarm     string `json:"goarm,omitempty"`
+	Goarm64   string `json:"goarm64,omitempty"`
+	Gomips    string `json:"gomips,omitempty"`
+	Goppc64   string `json:"goppc64,omitempty"`
+	Goriscv64 string `json:"goriscv64,omitempty"`
+	Type      Type   `json:"internal_type,omitempty"`
+	TypeS     string `json:"type,omitempty"`
+	Extra     Extras `json:"extra,omitempty"`
 }
 
 func (a Artifact) String() string {
