6109692be9c574aab13508b42405c2a35b1b1e0f
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-12-19 16:05:46 -0300

    fix(rust,zig): improve init, experimental warn (#5378)
    - adds common rust/cargo-zigbuild/zig folders/files to .gitignore on
    init
    - improves log output a little bit
    - avoid repeating "you are using the experimental Foo builder"

--- internal/builders/rust/build.go
+++ internal/builders/rust/build.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"slices"
 	"strings"
+	"sync"
 
 	"github.com/caarlos0/log"
 	"github.com/goreleaser/goreleaser/v2/internal/artifact"
@@ -80,9 +81,13 @@ func (b *Builder) Parse(target string) (api.Target, error) {
 	return t, nil
 }
 
+var once sync.Once
+
 // WithDefaults implements build.Builder.
 func (b *Builder) WithDefaults(build config.Build) (config.Build, error) {
-	log.Warn("you are using the experimental Rust builder")
+	once.Do(func() {
+		log.Warn("you are using the experimental Rust builder")
+	})
 
 	if len(build.Targets) == 0 {
 		build.Targets = defaultTargets()
