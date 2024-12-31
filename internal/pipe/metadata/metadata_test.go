d8b0f93a731ae88a23ce8b8a311b8aadabdd3132
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-12-31 14:08:25 -0300

    test: fix spurious 1*value

--- internal/pipe/metadata/metadata_test.go
+++ internal/pipe/metadata/metadata_test.go
@@ -27,7 +27,7 @@ func TestRunWithError(t *testing.T) {
 }
 
 func TestRun(t *testing.T) {
-	modTime := time.Now().AddDate(-1, 0, 0).Round(1 * time.Second).UTC()
+	modTime := time.Now().AddDate(-1, 0, 0).Round(time.Second).UTC()
 
 	getCtx := func(tmp string) *context.Context {
 		ctx := testctx.NewWithCfg(
