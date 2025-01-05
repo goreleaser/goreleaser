94c39ea2ae82167add2041078403c32901fc6cb8
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-01-05 18:50:03 -0300

    fix: lint

--- pkg/build/fail.go
+++ pkg/build/fail.go
@@ -25,10 +25,10 @@ type failBuilder struct {
 	err error
 }
 
-func (f failBuilder) WithDefaults(b config.Build) (config.Build, error)      { return b, f.err }
-func (f failBuilder) Build(*context.Context, config.Build, Options) error    { return f.err }
-func (f failBuilder) Parse(target string) (Target, error)                    { return nil, f.err }
-func (f failBuilder) Dependencies() []string                                 { return nil }
-func (f failBuilder) Prepare(ctx *context.Context, build config.Build) error { return f.err }
-func (f failBuilder) AllowConcurrentBuilds() bool                            { return false }
-func (f failBuilder) FixTarget(target string) string                         { return "" }
+func (f failBuilder) WithDefaults(b config.Build) (config.Build, error)   { return b, f.err }
+func (f failBuilder) Build(*context.Context, config.Build, Options) error { return f.err }
+func (f failBuilder) Parse(string) (Target, error)                        { return nil, f.err }
+func (f failBuilder) Dependencies() []string                              { return nil }
+func (f failBuilder) Prepare(*context.Context, config.Build) error        { return f.err }
+func (f failBuilder) AllowConcurrentBuilds() bool                         { return false }
+func (f failBuilder) FixTarget(string) string                             { return "" }
