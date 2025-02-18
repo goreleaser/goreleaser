d2b8ab6a50f7fca0f281c343171aa724917892ab
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-02-18 16:06:18 -0300

    feat: better support calver

--- internal/pipe/semver/semver_test.go
+++ internal/pipe/semver/semver_test.go
@@ -23,6 +23,17 @@ func TestValidSemver(t *testing.T) {
 	}, ctx.Semver)
 }
 
+func TestValidCalver(t *testing.T) {
+	ctx := testctx.New(testctx.WithCurrentTag("v2025.02.18-0rc1"))
+	require.NoError(t, Pipe{}.Run(ctx))
+	require.Equal(t, context.Semver{
+		Major:      2025,
+		Minor:      2,
+		Patch:      18,
+		Prerelease: "0rc1",
+	}, ctx.Semver)
+}
+
 func TestInvalidSemver(t *testing.T) {
 	ctx := testctx.New(testctx.WithCurrentTag("aaaav1.5.2-rc1"))
 	err := Pipe{}.Run(ctx)
