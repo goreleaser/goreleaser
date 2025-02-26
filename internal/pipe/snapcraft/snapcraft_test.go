4a246b6b4ab227274daaffb8d81b794252821087
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-02-25 22:42:29 -0300

    fix: snapcraft build filters

--- internal/pipe/snapcraft/snapcraft_test.go
+++ internal/pipe/snapcraft/snapcraft_test.go
@@ -495,7 +495,6 @@ func TestExtraFile(t *testing.T) {
 
 func TestDefault(t *testing.T) {
 	ctx := testctx.NewWithCfg(config.Project{
-		Builds: []config.Build{{ID: "foo"}},
 		Snapcrafts: []config.Snapcraft{{
 			Description: "hi",
 			Summary:     "hi",
@@ -503,7 +502,6 @@ func TestDefault(t *testing.T) {
 	})
 	require.NoError(t, Pipe{}.Default(ctx))
 	require.Equal(t, defaultNameTemplate, ctx.Config.Snapcrafts[0].NameTemplate)
-	require.Equal(t, []string{"foo"}, ctx.Config.Snapcrafts[0].Builds)
 	require.Equal(t, []string{"edge", "beta", "candidate", "stable"}, ctx.Config.Snapcrafts[0].ChannelTemplates)
 	require.Equal(t, "stable", ctx.Config.Snapcrafts[0].Grade)
 	require.Equal(t, "strict", ctx.Config.Snapcrafts[0].Confinement)
@@ -531,8 +529,7 @@ func TestDefaultNoSummary(t *testing.T) {
 
 func TestDefaultGradeTmpl(t *testing.T) {
 	ctx := testctx.NewWithCfg(config.Project{
-		Env:    []string{"Grade=devel"},
-		Builds: []config.Build{{ID: "foo"}},
+		Env: []string{"Grade=devel"},
 		Snapcrafts: []config.Snapcraft{
 			{
 				Grade:       "{{.Env.Grade}}",
@@ -543,7 +540,6 @@ func TestDefaultGradeTmpl(t *testing.T) {
 	})
 	require.NoError(t, Pipe{}.Default(ctx))
 	require.Equal(t, defaultNameTemplate, ctx.Config.Snapcrafts[0].NameTemplate)
-	require.Equal(t, []string{"foo"}, ctx.Config.Snapcrafts[0].Builds)
 	require.Equal(t, []string{"edge", "beta"}, ctx.Config.Snapcrafts[0].ChannelTemplates)
 	require.Equal(t, "devel", ctx.Config.Snapcrafts[0].Grade)
 }
