cf5f718b50cbde78b75330a9d0157d5fedb4a803
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-04-25 20:45:18 -0300

    fix(brew): improve if hardware structures
    closes https://github.com/orgs/goreleaser/discussions/5736

--- internal/pipe/brew/templates/linux_packages.rb
+++ internal/pipe/brew/templates/linux_packages.rb
@@ -1,32 +1,41 @@
 {{- define "linux_packages" }}
 {{- range $element := .LinuxPackages }}
   {{- if eq $element.Arch "amd64" }}
-  if Hardware::CPU.intel?
-    if Hardware::CPU.is_64_bit?
-  {{- end }}
-  {{- if or (eq $element.Arch "arm") (eq $element.Arch "arm64") }}
-  if Hardware::CPU.arm?
-  {{- if eq $element.Arch "arm64" }}
-    if Hardware::CPU.is_64_bit?
-  {{- else }}
-    if !Hardware::CPU.is_64_bit?
-  {{- end }}
-  {{- end }}
-      url "{{ $element.DownloadURL }}"
-      {{- if .DownloadStrategy }}, using: {{ .DownloadStrategy }}{{- end }}
-      {{- if .Headers }},
-        headers: [{{ printf "\n" }}
-          {{- join .Headers | indent 10 }}
-        ]
-      {{- end }}
-      sha256 "{{ $element.SHA256 }}"
-
-      def install
-        {{- range $index, $element := .Install }}
-        {{ . -}}
-        {{- end }}
-      end
+  if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
+    url "{{ $element.DownloadURL }}"
+    {{- if .DownloadStrategy }}, using: {{ .DownloadStrategy }}{{- end }}
+    {{- if .Headers }},
+    headers: [{{ printf "\n" }}
+      {{- join .Headers | indent 6 }}
+    ]
+    {{- end }}
+    sha256 "{{ $element.SHA256 }}"
+    def install
+    {{- range $index, $element := .Install }}
+      {{ . -}}
+    {{- end }}
     end
   end
+  {{- else if eq $element.Arch "arm64" }}
+  {{- if eq $element.Arch "arm64" }}
+  if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
+  {{- else }}
+  if Hardware::CPU.arm? and !Hardware::CPU.is_64_bit?
+  {{- end }}
+    url "{{ $element.DownloadURL }}"
+    {{- if .DownloadStrategy }}, using: {{ .DownloadStrategy }}{{- end }}
+    {{- if .Headers }},
+    headers: [{{ printf "\n" }}
+      {{- join .Headers | indent 6 }}
+    ]
+    {{- end }}
+    sha256 "{{ $element.SHA256 }}"
+    def install
+    {{- range $index, $element := .Install }}
+      {{ . -}}
+    {{- end }}
+    end
+  end
+{{- end }}
 {{- end }}
 {{- end }}
