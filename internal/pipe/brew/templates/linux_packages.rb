051b505170f41be1a9837ace20c3c2402b3faac7
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-04-28 08:52:07 -0300

    fix(brew): improve if hardware structures (#5741)

--- internal/pipe/brew/templates/linux_packages.rb
+++ internal/pipe/brew/templates/linux_packages.rb
@@ -1,31 +1,24 @@
 {{- define "linux_packages" }}
 {{- range $element := .LinuxPackages }}
   {{- if eq $element.Arch "amd64" }}
-  if Hardware::CPU.intel?
-    if Hardware::CPU.is_64_bit?
+  if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
+  {{- else if eq $element.Arch "arm64" }}
+  if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
+  {{- else if eq $element.Arch "arm" }}
+  if Hardware::CPU.arm? and !Hardware::CPU.is_64_bit?
   {{- end }}
-  {{- if or (eq $element.Arch "arm") (eq $element.Arch "arm64") }}
-  if Hardware::CPU.arm?
-  {{- if eq $element.Arch "arm64" }}
-    if Hardware::CPU.is_64_bit?
-  {{- else }}
-    if !Hardware::CPU.is_64_bit?
-  {{- end }}
-  {{- end }}
-      url "{{ $element.DownloadURL }}"
-      {{- if .DownloadStrategy }}, using: {{ .DownloadStrategy }}{{- end }}
-      {{- if .Headers }},
-        headers: [{{ printf "\n" }}
-          {{- join .Headers | indent 10 }}
-        ]
-      {{- end }}
-      sha256 "{{ $element.SHA256 }}"
-
-      def install
-        {{- range $index, $element := .Install }}
-        {{ . -}}
-        {{- end }}
-      end
+    url "{{ $element.DownloadURL }}"
+    {{- if .DownloadStrategy }}, using: {{ .DownloadStrategy }}{{- end }}
+    {{- if .Headers }},
+      headers: [{{ printf "\n" }}
+        {{- join .Headers | indent 8 }}
+      ]
+    {{- end }}
+    sha256 "{{ $element.SHA256 }}"
+    def install
+    {{- range $index, $element := .Install }}
+      {{ . -}}
+    {{- end }}
     end
   end
 {{- end }}
