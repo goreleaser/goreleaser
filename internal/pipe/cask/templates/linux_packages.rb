efb6dab8505cefdecb6522eae4bbacc26e1e6d29
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-05-22 13:08:41 -0300

    fix: template

--- internal/pipe/cask/templates/linux_packages.rb
+++ internal/pipe/cask/templates/linux_packages.rb
@@ -4,8 +4,6 @@
   on_intel do
   {{- else if eq $element.Arch "arm64" }}
   on_arm do
-  {{- else if eq $element.Arch "arm" }}
-  if Hardware::CPU.arm? and !Hardware::CPU.is_64_bit?
   {{- end }}
     url "{{ $element.DownloadURL }}"
     sha256 "{{ $element.SHA256 }}"
