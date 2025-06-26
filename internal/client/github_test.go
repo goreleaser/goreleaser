49696be4fbde64cd8a999aac7edab6ece2e92c1f
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-06-26 16:43:21 -0300

    chore(deps): update go-github to v73

--- internal/client/github_test.go
+++ internal/client/github_test.go
@@ -13,7 +13,7 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/google/go-github/v72/github"
+	"github.com/google/go-github/v73/github"
 	"github.com/goreleaser/goreleaser/v2/internal/artifact"
 	"github.com/goreleaser/goreleaser/v2/internal/testctx"
 	"github.com/goreleaser/goreleaser/v2/internal/testlib"
