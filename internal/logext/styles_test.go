9e466d9daea1d4da41f25cef840e663e17b4ee54
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-07-02 20:58:23 -0300

    feat: log duration of all shell commands (#5872)
    also improves styling of duration logs

---
+++ internal/logext/styles_test.go
@@ -0,0 +1,24 @@
+package logext
+
+import (
+	"bytes"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/caarlos0/log"
+	"github.com/goreleaser/goreleaser/v2/internal/golden"
+)
+
+func TestDuration(t *testing.T) {
+	t.Cleanup(func() {
+		log.Log = log.New(os.Stderr)
+	})
+	var b bytes.Buffer
+	log.Log = log.New(&b)
+	log.Info("before")
+	Duration(time.Now().Add(-10*time.Second), time.Minute)
+	Duration(time.Now().Add(-10*time.Minute), time.Minute)
+	log.Info("after")
+	golden.RequireEqualTxt(t, b.Bytes())
+}
