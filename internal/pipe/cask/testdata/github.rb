8f025d7ad772985ff1b6b9bb8363d1676a1ae2da
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-08-01 20:27:33 +0900

    use raw net http to retrieve api token from self.token
    Co-authored-by: Tokkyer Midlow <mltokky@gmail.com>

--- internal/pipe/cask/testdata/github.rb
+++ internal/pipe/cask/testdata/github.rb
@@ -1,10 +1,4 @@
 module GitHubHelper
-  def self.get_asset_api_url(tag, name)
-    require "utils/github"
-    release = GitHub.get_release("goreleaser", "example", tag)
-    release["assets"].find { |asset| asset["name"] == name }["url"]
-  end
-
   def self.token
     require "utils/github"
     @github_token = ENV["HOMEBREW_GITHUB_API_TOKEN"]
@@ -13,5 +7,23 @@ module GitHubHelper
       raise "Failed to retrieve token" if @github_token.nil? || @github_token.empty?
     end
     @github_token
+  end
+
+  def self.get_asset_api_url(tag, name)
+    require "json"
+    require "net/http"
+    require "uri"
+
+    resp = Net::HTTP.get(
+      URI.parse("https://api.github.com/repos/goreleaser/example/releases/tags/#{tag}"),
+      {
+        "Accept" => "application/vnd.github+json",
+        "Authorization" => "Bearer #{token}",
+        "X-GitHub-Api-Version" => "2022-11-28"
+      }
+    )
+
+    release = JSON.parse(resp)
+    release["assets"].find { |asset| asset["name"] == name }["url"]
   end
 end
