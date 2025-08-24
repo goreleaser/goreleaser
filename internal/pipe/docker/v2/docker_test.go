158a2ebb6b421af99ea529cbd7151a0ac23b73ad
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-08-24 00:49:41 -0300

    test: fix
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/pipe/docker/v2/docker_test.go
+++ internal/pipe/docker/v2/docker_test.go
@@ -203,10 +203,6 @@ func TestMakeArgs(t *testing.T) {
 
 func TestToPlatform(t *testing.T) {
 	for expected, art := range map[string]artifact.Artifact{
-		"darwin/amd64": {
-			Goos:   "darwin",
-			Goarch: "amd64",
-		},
 		"darwin/arm64": {
 			Goos:   "darwin",
 			Goarch: "arm64",
