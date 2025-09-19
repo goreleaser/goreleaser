a2afb79860942b3256e487b76fa7e65524dd53d4
kongfei605 <kongfei605@gmail.com>
2025-09-19 16:17:12 +0800

    chore: global hooks after build

--- internal/pipeline/pipeline.go
+++ internal/pipeline/pipeline.go
@@ -75,8 +75,6 @@ var BuildPipeline = []Piper{
 	snapshot.Pipe{},
 	// run global hooks before build
 	before.Pipe{},
-	// run global hooks after build
-	after.Pipe{},
 	// ensure ./dist exists and is empty
 	dist.Pipe{},
 	// setup metadata options
@@ -112,6 +110,8 @@ var BuildCmdPipeline = append(
 	BuildPipeline,
 	reportsizes.Pipe{},
 	metadata.ArtifactsPipe{},
+	// run global hooks after build
+	after.Pipe{},
 )
 
 // Pipeline contains all pipe implementations in order.
