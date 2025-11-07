6931cb1b9abf2d8524c330d0eca7be661ed6923b
Marvin Drees <marvin.drees@9elements.com>
2025-11-07 14:20:30 +0100

    fix: adapt internal/pipe for new GOARM targets
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- internal/pipe/nfpm/nfpm_test.go
+++ internal/pipe/nfpm/nfpm_test.go
@@ -427,7 +427,12 @@ func TestRunPipe(t *testing.T) {
 		require.Equal(t, "."+pkg.Format(), pkg.Ext())
 		arch := pkg.Goarch
 		if pkg.Goarm != "" {
-			arch += "v" + pkg.Goarm
+			// Normalize goarm to handle comma-separated values like "6,softfloat"
+			normalizedGoarm := pkg.Goarm
+			if idx := strings.Index(normalizedGoarm, ","); idx > 0 {
+				normalizedGoarm = normalizedGoarm[:idx]
+			}
+			arch += "v" + normalizedGoarm
 		}
 		if pkg.Goamd64 != "v1" {
 			arch += pkg.Goamd64
@@ -468,13 +473,19 @@ func TestRunPipe(t *testing.T) {
 			require.Equal(t, now.UTC(), src.FileInfo.MTime.UTC(), src.Destination)
 		}
 
+		// Normalize goarm to handle comma-separated values like "6,softfloat"
+		normalizedGoarm := pkg.Goarm
+		if idx := strings.Index(normalizedGoarm, ","); idx > 0 {
+			normalizedGoarm = normalizedGoarm[:idx]
+		}
+
 		require.ElementsMatch(t, []string{
 			"./testdata/testfile.txt",
 			"./testdata/testfile.txt",
 			"./testdata/testfile.txt",
 			"/etc/nope.conf",
 			"./testdata/folder",
-			"./testdata/testfile-" + pkg.Goarch + pkg.Goamd64 + pkg.Goarm + pkg.Gomips + ".txt",
+			"./testdata/testfile-" + pkg.Goarch + pkg.Goamd64 + normalizedGoarm + pkg.Gomips + ".txt",
 			binPath,
 			foohPath,
 			fooaPath,
