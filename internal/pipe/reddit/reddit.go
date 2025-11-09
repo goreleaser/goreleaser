35b54d7d7a3f6cd3a46f83e58ede21aa7923e4cd
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2025-11-08 23:25:13 -0500

    fix: dynamically use announcer names in errors
    This PR uses this setup in announcer error messages. It also fixes an
    error where the Mattermost announcer mistakenly refers to itself as
    Teams. Creating this PR was brought up in #6199.

--- internal/pipe/reddit/reddit.go
+++ internal/pipe/reddit/reddit.go
@@ -41,15 +41,15 @@ func (Pipe) Default(ctx *context.Context) error {
 	return nil
 }
 
-func (Pipe) Announce(ctx *context.Context) error {
+func (p Pipe) Announce(ctx *context.Context) error {
 	title, err := tmpl.New(ctx).Apply(ctx.Config.Announce.Reddit.TitleTemplate)
 	if err != nil {
-		return fmt.Errorf("reddit: %w", err)
+		return fmt.Errorf("%s: %w", p, err)
 	}
 
 	url, err := tmpl.New(ctx).Apply(ctx.Config.Announce.Reddit.URLTemplate)
 	if err != nil {
-		return fmt.Errorf("reddit: %w", err)
+		return fmt.Errorf("%s: %w", p, err)
 	}
 
 	linkRequest := reddit.SubmitLinkRequest{
@@ -60,18 +60,18 @@ func (Pipe) Announce(ctx *context.Context) error {
 
 	cfg, err := env.ParseAs[Config]()
 	if err != nil {
-		return fmt.Errorf("reddit: %w", err)
+		return fmt.Errorf("%s: %w", p, err)
 	}
 
 	credentials := reddit.Credentials{ID: ctx.Config.Announce.Reddit.ApplicationID, Secret: cfg.Secret, Username: ctx.Config.Announce.Reddit.Username, Password: cfg.Password}
 	client, err := reddit.NewClient(credentials)
 	if err != nil {
-		return fmt.Errorf("reddit: %w", err)
+		return fmt.Errorf("%s: %w", p, err)
 	}
 
 	post, _, err := client.Post.SubmitLink(ctx, linkRequest)
 	if err != nil {
-		return fmt.Errorf("reddit: %w", err)
+		return fmt.Errorf("%s: %w", p, err)
 	}
 
 	log.Infof("The text post is available at: %s\n", post.URL)
