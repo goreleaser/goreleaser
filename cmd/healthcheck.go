5466be4a8e14de9ebc9d8d0a73100c65d5fca354
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-11-09 22:03:09 -0300

    chore: fmt
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- cmd/healthcheck.go
+++ cmd/healthcheck.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/caarlos0/log"
 	"charm.land/lipgloss/v2"
+	"github.com/caarlos0/log"
 	"github.com/goreleaser/goreleaser/v2/internal/middleware/skip"
 	"github.com/goreleaser/goreleaser/v2/internal/pipe/defaults"
 	"github.com/goreleaser/goreleaser/v2/pkg/context"
