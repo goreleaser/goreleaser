4f4893f40d376a4bd2469573d346bcd289015963
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-02-17 18:23:08 -0300

    fixup! sec: redact secrets from command outputs and logs
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/pipe/docker/api.go
+++ internal/pipe/docker/api.go
@@ -74,7 +74,7 @@ func runCommandWithOutput(ctx *context.Context, dir, binary string, args ...stri
 
 	var b bytes.Buffer
 	w := gio.Safe(&b)
-	cmd.Stderr = io.MultiWriter(logext.NewWriter(), w)
+	cmd.Stderr = redact.Writer(io.MultiWriter(logext.NewWriter(), w), cmd.Env)
 
 	log.
 		WithField("cmd", append([]string{binary}, args[0])).
@@ -84,7 +84,7 @@ func runCommandWithOutput(ctx *context.Context, dir, binary string, args ...stri
 	out, err := cmd.Output()
 	if out != nil {
 		// regardless of command success, always print stdout for backward-compatibility with runCommand()
-		_, _ = io.MultiWriter(logext.NewWriter(), w).Write(out)
+		_, _ = redact.Writer(io.MultiWriter(logext.NewWriter(), w), cmd.Env).Write(out)
 	}
 	if err != nil {
 		return nil, fmt.Errorf("%w: %s", err, b.String())
