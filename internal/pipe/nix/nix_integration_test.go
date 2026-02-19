aeba33744b30427be409a0f438f45325d09d658b
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-02-19 12:20:05 -0300

    test: nix formatter tests
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/pipe/nix/nix_integration_test.go
+++ internal/pipe/nix/nix_integration_test.go
@@ -3,8 +3,11 @@
 package nix
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/goreleaser/goreleaser/v2/internal/golden"
 	"github.com/goreleaser/goreleaser/v2/internal/testctx"
 	"github.com/goreleaser/goreleaser/v2/internal/testlib"
 	"github.com/goreleaser/goreleaser/v2/pkg/config"
@@ -31,4 +34,58 @@ func TestIntegrationHasherAvailableValid(t *testing.T) {
 	testlib.CheckPath(t, "nix-hash")
 	testlib.SkipIfWindows(t, "nix doesn't work on windows")
 	require.True(t, realHasher.Available())
+}
+
+func TestFormat(t *testing.T) {
+	testlib.SkipIfWindows(t, "nix.format won't work on Windows")
+
+	t.Run("invalid formatter", func(t *testing.T) {
+		ctx := testctx.Wrap(t.Context())
+		require.False(t, format(ctx, "invalid-formatter", "nope.nix"))
+	})
+
+	const input = `{  foo = "bar";
+							baz = "qux";	}`
+
+	t.Run("alejandra", func(t *testing.T) {
+		testlib.CheckPath(t, "alejandra")
+
+		ctx := testctx.Wrap(t.Context())
+		path := filepath.Join(t.TempDir(), "test.nix")
+		require.NoError(t, os.WriteFile(path, []byte(input), 0o644))
+
+		require.True(t, format(ctx, "alejandra", path))
+
+		content, err := os.ReadFile(path)
+		require.NoError(t, err)
+		golden.RequireEqualExt(t, content, ".nix")
+	})
+
+	t.Run("nixfmt", func(t *testing.T) {
+		testlib.CheckPath(t, "nixfmt")
+
+		ctx := testctx.Wrap(t.Context())
+		path := filepath.Join(t.TempDir(), "test.nix")
+		require.NoError(t, os.WriteFile(path, []byte(input), 0o644))
+
+		require.True(t, format(ctx, "nixfmt", path))
+
+		content, err := os.ReadFile(path)
+		require.NoError(t, err)
+		golden.RequireEqualExt(t, content, ".nix")
+	})
+
+	t.Run("invalid file", func(t *testing.T) {
+		testlib.CheckPath(t, "nixfmt")
+
+		ctx := testctx.Wrap(t.Context())
+		path := filepath.Join(t.TempDir(), "test.nix")
+		require.NoError(t, os.WriteFile(path, []byte(`{ invalid file`), 0o644))
+
+		require.False(t, format(ctx, "nixfmt", path))
+
+		content, err := os.ReadFile(path)
+		require.NoError(t, err)
+		golden.RequireEqualExt(t, content, ".nix")
+	})
 }
