d19a9afdd548ec247a76545674307f047398d45d
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-02-20 15:52:14 -0300

    fix: syft test
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/pipe/sbom/testdata/fakesyft.bat
+++ internal/pipe/sbom/testdata/fakesyft.bat
@@ -2,11 +2,15 @@
 setlocal enabledelayedexpansion
 
 rem Parse args to find output files from --output spdx-json=<path> patterns
-for %%a in (%*) do (
-    set "arg=%%a"
-    if "!arg:~0,10!"=="spdx-json=" (
-        set "outfile=!arg:~10!"
-        echo {"spdxVersion":"SPDX-2.3","name":"fake"} > "!outfile!"
-        echo Fake cataloged: !outfile!
-    )
+rem Note: for %%a in (%*) splits on = so we must use shift-based parsing
+:loop
+if "%~1"=="" goto :done
+set "arg=%~1"
+if "!arg:~0,10!"=="spdx-json=" (
+    set "outfile=!arg:~10!"
+    echo {"spdxVersion":"SPDX-2.3","name":"fake"} > "!outfile!"
+    echo Fake cataloged: !outfile!
 )
+shift
+goto :loop
+:done
