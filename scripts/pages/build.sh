f789d0bcd3f653927c64aba32e81c124d7717bb9
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-03-21 22:53:20 -0300

    build
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- scripts/pages/build.sh
+++ scripts/pages/build.sh
@@ -4,13 +4,9 @@ set -euo pipefail
 # unshallow (needed for the rss plugin)
 git fetch --prune --tags --unshallow
 
-# install
-pip install --upgrade pip
-pip install -r ./www/requirements.txt
-
 # prepare
-version="$(cat ./www/docs/static/latest)"
-sed -s'' -i "s/__VERSION__/$version/g" www/docs/install.md www/docs/customization/index.md
+version="$(cat ./www/static/latest)"
+sed -i "s/__VERSION__/$version/g" www/content/getting-started/install.md
 
 # build
-mkdocs build -f www/mkdocs.yml
+cd www && hugo --gc --minify
