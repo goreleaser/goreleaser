7b6496fdc4f869b09d4bca331f938bf67a06ffcd
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-05-19 09:25:20 -0300

    docs: better install page (#6631)
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- scripts/pages/build.sh
+++ scripts/pages/build.sh
@@ -6,7 +6,11 @@ git fetch --prune --tags --unshallow
 
 # prepare
 version="$(cat ./www/static/latest)"
-sed -i'' "s/__VERSION__/$version/g" www/content/getting-started/install.md www/content/customization/_index.md
+sed -i'' "s/__VERSION__/$version/g" \
+  www/content/getting-started/install/_index.md \
+  www/content/getting-started/install/oss.md \
+  www/content/getting-started/install/pro.md \
+  www/content/customization/_index.md
 
 # build
 cd www && hugo --gc --minify
